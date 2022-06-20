ba685445a800907fa1192d68008897b130fa8675
koushenhai <2413176044@qq.com>
2022-06-20 12:53:06 +0800

    优化代码

--- laokou-base/laokou-common/src/main/java/io/laokou/common/user/UserDetail.java
+++ laokou-base/laokou-common/src/main/java/io/laokou/common/user/UserDetail.java
@@ -1,6 +1,4 @@
 package io.laokou.common.user;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import java.io.Serializable;
 import java.util.List;
@@ -10,7 +8,6 @@ import java.util.List;
 @Data
 public class UserDetail implements Serializable {
     private static final long serialVersionUID = 1L;
-    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private String username;
     private String imgUrl;
@@ -19,9 +16,7 @@ public class UserDetail implements Serializable {
     private String email;
     private String mobile;
     private String password;
-    private String wxOpenid;
     private String zfbOpenid;
-    private String wxgzhOpenid;
     private List<Long> roleIds;
     private List<String> permissionsList;
 
