060e75928b8868727b64cabdaf50c78409345211
koushenhai <2413176044@qq.com>
2022-06-25 14:00:06 +0800

    优化代码

--- deploy/laokou-register.sh
+++ deploy/laokou-register.sh
@@ -1,1 +1,1 @@
-cd /opt/laokou && nohup java -jar register.jar --spring.profiles.active=prod > register.log &
+cd /opt/laokou && nohup java -Xms256m -Xmx512m -jar register.jar --spring.profiles.active=prod > register.log &
