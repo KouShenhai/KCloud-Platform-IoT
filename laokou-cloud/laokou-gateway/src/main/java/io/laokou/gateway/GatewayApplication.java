60a8fb9d01735d68c82a4c5ceadc5b15d63c8301
koushenhai <2413176044@qq.com>
2022-07-02 13:47:54 +0800

    修改配置文件

--- laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/GatewayApplication.java
+++ laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package io.laokou.gateway;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableEncryptableProperties
 public class GatewayApplication {
 
     public static void main(String[] args) {
---
+++ laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/config/JasyptConfig.java
@@ -0,0 +1,37 @@
+package io.laokou.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 加密
+ * @author Kou Shenhai
+ * @version 1.0
+ * @date 2020/8/13 0013 下午 10:13
+ */
+
+@Configuration
+@Slf4j
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String passwordEncode;
+
+    @Bean
+    public StringEncryptor stringEncryptor() {
+        log.info("秘钥：{}",passwordEncode);
+        //加密
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        EnvironmentStringPBEConfig config = new EnvironmentStringPBEConfig();
+        //应用配置
+        config.setPassword(passwordEncode);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
