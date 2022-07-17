daa32df41bbb8e08f40ec4a5f01a3c4117a3ede9
koushenhai <2413176044@qq.com>
2022-07-17 13:59:45 +0800

    新增auth部署shell脚本

---
+++ deploy/laokou-auth.sh
@@ -0,0 +1,1 @@
+cd /opt/laokou && sudo nohup java -Xms256m -Xmx512m -Ddruid.filters=mergeStat -Ddruid.useGlobalDataSourceStat=true -Djava.library.path=/usr/local/apr/lib -Djasypt.encryptor.password=koushenhai5201314wumeihua -Denv=DEV -Dapollo.configService=http://124.222.196.51:8080 -jar auth.jar --spring.profiles.active=prod > auth.log &
