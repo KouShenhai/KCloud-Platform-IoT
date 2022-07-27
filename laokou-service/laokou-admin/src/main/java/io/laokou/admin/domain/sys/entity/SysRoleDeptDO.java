fc928bde546477248534efeb10308665f47f0d98
koushenhai <2413176044@qq.com>
2022-07-27 22:55:57 +0800

    优化sql文件

--- laokou-service/laokou-admin/src/main/java/io/laokou/admin/domain/sys/entity/SysRoleDeptDO.java
+++ laokou-service/laokou-admin/src/main/java/io/laokou/admin/domain/sys/entity/SysRoleDeptDO.java
@@ -1,5 +1,7 @@
 package io.laokou.admin.domain.sys.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 /**
@@ -8,6 +10,8 @@ import lombok.Data;
  * @date 2022/7/27 0027 上午 9:20
  */
 @Data
+@TableName("boot_sys_role_dept")
+@ApiModel("系统角色部门DO")
 public class SysRoleDeptDO {
 
     private Long roleId;
