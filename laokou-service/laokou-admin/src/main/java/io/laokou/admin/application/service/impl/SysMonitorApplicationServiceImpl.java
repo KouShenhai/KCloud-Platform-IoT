bb0ce73214b7db08bc53b2610c70116abb8107f3
koushenhai <2413176044@qq.com>
2022-07-28 23:33:29 +0800

    优化代码

--- laokou-service/laokou-admin/src/main/java/io/laokou/admin/application/service/impl/SysMonitorApplicationServiceImpl.java
+++ laokou-service/laokou-admin/src/main/java/io/laokou/admin/application/service/impl/SysMonitorApplicationServiceImpl.java
@@ -1,6 +1,7 @@
 package io.laokou.admin.application.service.impl;
 import io.laokou.admin.application.service.SysMonitorApplicationService;
 import io.laokou.admin.interfaces.vo.CacheVO;
+import io.laokou.admin.interfaces.vo.ServerVO;
 import io.laokou.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,13 @@ public class SysMonitorApplicationServiceImpl implements SysMonitorApplicationSe
         vo.setCommandStats(redisUtil.getCommandStatus());
         vo.setInfo(redisUtil.getInfo());
         vo.setKeysSize(redisUtil.getKeysSize());
+        return vo;
+    }
+
+    @Override
+    public ServerVO getServerInfo() throws Exception {
+        ServerVO vo = new ServerVO();
+        vo.copyTo();
         return vo;
     }
 }
