e3703654d4a5a008ea0beeb6156b23c18003d535
koushenhai <2413176044@qq.com>
2022-08-10 11:10:15 +0800

    新增oauth2

--- laokou-service/laokou-oauth2/src/main/java/io/laokou/oauth2/Oauth2Application.java
+++ laokou-service/laokou-oauth2/src/main/java/io/laokou/oauth2/Oauth2Application.java
@@ -1,8 +1,6 @@
 package io.laokou.oauth2;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author Kou Shenhai
  */
