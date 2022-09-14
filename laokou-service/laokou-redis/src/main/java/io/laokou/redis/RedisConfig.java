afa8aca1b33cc1ada9e7dc19d5728346cae75c05
koushenhai <2413176044@qq.com>
2022-09-14 18:50:24 +0800

    优化代码

--- laokou-service/laokou-redis/src/main/java/io/laokou/redis/RedisConfig.java
+++ laokou-service/laokou-redis/src/main/java/io/laokou/redis/RedisConfig.java
@@ -1,9 +1,14 @@
 package io.laokou.redis;
-import io.laokou.redis.serializer.JsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import javax.annotation.Resource;
 /**
@@ -18,11 +23,24 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new JsonRedisSerializer<>(Object.class));
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new JsonRedisSerializer<>(Object.class));
         redisTemplate.setConnectionFactory(factory);
+        //Json序列化配置
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance , ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        //String序列化
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        //key
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        //value
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        //hash-key
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        //hash-value
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }
