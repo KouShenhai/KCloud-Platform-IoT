405ff0e60375a1cdf6e09f5ea1356b561f544965
koushenhai <2413176044@qq.com>
2022-09-20 13:22:59 +0800

    升级2.7.3版本中（请回退到 版本 481019718d42802ff6e514efd327cebd48538765）

--- laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/config/RouterFunctionConfig.java
+++ laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/config/RouterFunctionConfig.java
@@ -1,13 +1,20 @@
 package io.laokou.gateway.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import io.laokou.gateway.component.Resilience4jFallbackHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import java.time.Duration;
 
 @Configuration
 public class RouterFunctionConfig {
@@ -18,6 +25,16 @@ public class RouterFunctionConfig {
     @Bean
     public RouterFunction routerFunction() {
         return RouterFunctions.route(RequestPredicates.path("/fallback").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), resilience4jFallbackHandler);
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                // 默认超时时间 4s
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                // circuitBreaker 使用默认配置
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .build());
     }
 
 }
