53f9ee8b5fbb74801326bd06a40d84d8a6d8fe67
koushenhai <2413176044@qq.com>
2022-09-21 11:30:40 +0800

    增加开源协议

--- laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/config/RouterFunctionConfig.java
+++ laokou-cloud/laokou-gateway/src/main/java/io/laokou/gateway/config/RouterFunctionConfig.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020-2022 Kou Shenhai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.laokou.gateway.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
@@ -12,9 +27,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-
-import java.time.Duration;
-
 @Configuration
 public class RouterFunctionConfig {
 
