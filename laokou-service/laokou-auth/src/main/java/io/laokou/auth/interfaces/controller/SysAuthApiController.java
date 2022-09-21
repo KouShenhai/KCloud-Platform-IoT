5c6124e5838d55dbd75b58ece919ad170b2f9979
koushenhai <2413176044@qq.com>
2022-09-21 12:26:24 +0800

    增加开源协议

--- laokou-service/laokou-auth/src/main/java/io/laokou/auth/interfaces/controller/SysAuthApiController.java
+++ laokou-service/laokou-auth/src/main/java/io/laokou/auth/interfaces/controller/SysAuthApiController.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020-2022 Kou Shenhai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.laokou.auth.interfaces.controller;
 import io.laokou.auth.application.service.SysAuthApplicationService;
 import io.laokou.auth.interfaces.dto.LoginDTO;
@@ -5,7 +20,6 @@ import io.laokou.common.user.BaseUserVO;
 import io.laokou.auth.interfaces.vo.LoginVO;
 import io.laokou.auth.interfaces.vo.UserInfoVO;
 import io.laokou.common.constant.Constant;
-import io.laokou.common.exception.CustomException;
 import io.laokou.common.user.SecurityUser;
 import io.laokou.common.user.UserDetail;
 import io.laokou.common.utils.HttpResultUtil;
