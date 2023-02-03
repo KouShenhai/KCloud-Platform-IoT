de5e18811695c7da4fb5b5dd10a00799619d1e68
koushenhai <2413176044@qq.com>
2023-02-03 12:01:04 +0800

    refactor: saas数据库提交

--- laokou-common/laokou-common-mybatis-plus/src/main/java/org/laokou/common/mybatisplus/config/MybatisPlusConfig.java
+++ laokou-common/laokou-common-mybatis-plus/src/main/java/org/laokou/common/mybatisplus/config/MybatisPlusConfig.java
@@ -15,20 +15,16 @@
  */
 package org.laokou.common.mybatisplus.config;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
-import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.LongValue;
 import org.laokou.common.core.utils.HttpContextUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import static org.laokou.common.core.constant.Constant.TENANT_ID;
-
 /**
  * mybatis-plus的配置
  * @author laokou
