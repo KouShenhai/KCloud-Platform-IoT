d5948b7f8908e845ceee29040f5d06bcfab3d73c
koushenhai <2413176044@qq.com>
2023-02-10 19:13:35 +0800

    refactor: 修改token过期的提示

--- laokou-common/laokou-common-swagger/src/main/java/org/laokou/common/swagger/exception/ErrorCode.java
+++ laokou-common/laokou-common-swagger/src/main/java/org/laokou/common/swagger/exception/ErrorCode.java
@@ -115,7 +115,7 @@ public interface ErrorCode {
      */
     int AUTHORIZATION_NOT_NULL = 10020;
     /**
-     * token已过期，请重新登录
+     * 登录状态已过期，请重新登录
      */
     int AUTHORIZATION_INVALID = 10021;
     /**
