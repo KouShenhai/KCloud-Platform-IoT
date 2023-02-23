a6e2d31345ea3e668146c6b79cc5ccf7115dc206
koushenhai <2413176044@qq.com>
2023-02-23 13:14:47 +0800

    refactor: 修改md

--- doc/db/db_update.sql
+++ doc/db/db_update.sql
@@ -6,3 +6,7 @@ alter table boot_sys_user drop idx_tenant_id;
 alter table boot_sys_user add unique index idx_tenant_id_username(tenant_id, username) comment '租户_用户名_唯一索引';
 alter table boot_sys_user add unique index idx_tenant_id_mail(tenant_id, mail) comment '租户_邮箱_唯一索引';
 alter table boot_sys_user add unique index idx_tenant_id_mobile(tenant_id, mobile) comment '租户_手机号_唯一索引';
+
+-- 2023/2/23 增加oauth2_authorization索引 老寇
+alter table oauth2_authorization add index idx_token_expires_issued_principal_name(access_token_value(700),access_token_expires_at,access_token_issued_at,principal_name) comment 'token_过期时间_开始时间_登录人_索引';
+
