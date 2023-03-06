371775aa376f79ac0d6bbf5c55892a2c4ad3a485
koushenhai <2413176044@qq.com>
2023-03-06 15:15:19 +0800

    refactor: 多租户增加表检查

--- doc/db/db_tenant_init.sql
+++ doc/db/db_tenant_init.sql
@@ -76,24 +76,20 @@ CREATE TABLE `boot_sys_oss` (
                                 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='对象存储';
 
-CREATE TABLE `boot_sys_oss` (
-                                `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
-                                `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
-                                `editor` bigint(20) DEFAULT NULL COMMENT '编辑人',
-                                `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                                `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-                                `del_flag` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1已删除 0未删除',
-                                `name` varchar(20) DEFAULT NULL COMMENT '名称',
-                                `endpoint` varchar(200) DEFAULT NULL COMMENT '终端地址',
-                                `region` varchar(10) DEFAULT NULL COMMENT '区域',
-                                `access_key` varchar(50) DEFAULT NULL COMMENT '访问密钥',
-                                `secret_key` varchar(100) DEFAULT NULL COMMENT '用户密钥',
-                                `bucket_name` varchar(20) DEFAULT NULL COMMENT '桶名',
-                                `path_style_access_enabled` tinyint(1) DEFAULT NULL COMMENT '路径样式访问 1已开启 0未启用',
-                                `status` tinyint(1) DEFAULT NULL COMMENT '状态 1已启用 0未启用',
-                                `version` int(11) NOT NULL DEFAULT '0' COMMENT '版本号',
-                                PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='对象存储';
+CREATE TABLE `boot_sys_oss_log` (
+                                    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                    `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
+                                    `editor` bigint(20) DEFAULT NULL COMMENT '编辑人',
+                                    `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                    `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+                                    `del_flag` tinyint(1) DEFAULT '0' COMMENT '1已删除 0未删除',
+                                    `md5` varchar(100) DEFAULT NULL COMMENT 'md5标识',
+                                    `url` varchar(200) DEFAULT NULL COMMENT '路径',
+                                    `file_name` varchar(100) DEFAULT NULL COMMENT '文件名称',
+                                    `file_size` bigint(20) DEFAULT NULL COMMENT '文件大小',
+                                    `version` int(11) NOT NULL DEFAULT '0' COMMENT '版本号',
+                                    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='存储日志';
 
 -- 2023/2/24 增加boot_sys_message_detail索引 老寇
 ALTER table boot_sys_message_detail add INDEX idx_read_flag_user_id(read_flag,user_id) COMMENT '已读_用户编号_索引';
