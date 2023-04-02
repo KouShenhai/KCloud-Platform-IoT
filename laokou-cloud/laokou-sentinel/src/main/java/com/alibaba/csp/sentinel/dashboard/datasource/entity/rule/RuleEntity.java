b9609c4436dc8df06d25091b9dd2177c789c7fb2
koushenhai <2413176044@qq.com>
2023-04-02 14:39:46 +0800

    feat: 项目引入sentinel服务

--- laokou-cloud/laokou-sentinel/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/rule/RuleEntity.java
+++ laokou-cloud/laokou-sentinel/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/rule/RuleEntity.java
@@ -1,26 +1,40 @@
-/**
- * Copyright (c) 2022 KCloud-Platform-Alibaba Authors. All Rights Reserved.
- * <p>
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.alibaba.csp.sentinel.dashboard.datasource.entity.rule;
 
-package org.laokou.admin.server.infrastructure.util;
+import java.util.Date;
 
-import org.springframework.stereotype.Component;
+import com.alibaba.csp.sentinel.slots.block.Rule;
+
 /**
- * @author laokou
+ * @author leyou
  */
-@Component
-public class MessageUtil {
+public interface RuleEntity {
 
+    Long getId();
+
+    void setId(Long id);
+
+    String getApp();
+
+    String getIp();
+
+    Integer getPort();
+
+    Date getGmtCreate();
+    
+    Rule toRule();
 }
