c36b06d6a2afd776a7895c94ddf7c4a6d14a9963
koushenhai <2413176044@qq.com>
2023-05-09 00:21:39 +0800

    refactor: 优化代码

---
+++ laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/main/java/org/laokou/test/elasticsearch/ElasticsearchTestApplication.java
@@ -0,0 +1,13 @@
+package org.laokou.test.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchTestApplication.class, args);
+    }
+
+}
---
+++ laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/test/java/org/laokou/test/elasticsearch/ElasticsearchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laokou.test.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
