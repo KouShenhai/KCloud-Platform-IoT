e784fa3090972fd82eea65486bfc090caff2c408
koushenhai <2413176044@qq.com>
2023-05-09 08:19:20 +0800

    feat: 升级es api至 8.6.2

--- laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/main/java/org/laokou/test/elasticsearch/ElasticsearchTestApplication.java
+++ laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/main/java/org/laokou/test/elasticsearch/ElasticsearchTestApplication.java
@@ -1,9 +1,14 @@
 package org.laokou.test.elasticsearch;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author laokou
+ */
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ElasticsearchTestApplication {
 
     public static void main(String[] args) {
--- laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/test/java/org/laokou/test/elasticsearch/ElasticsearchTestApplicationTests.java
+++ laokou-service/laokou-module/laokou-test/laokou-test-elasticsearch/src/test/java/org/laokou/test/elasticsearch/ElasticsearchTestApplicationTests.java
@@ -1,13 +1,28 @@
 package org.laokou.test.elasticsearch;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.laokou.common.elasticsearch.template.NewElasticsearchTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+
+import java.util.List;
 
 @SpringBootTest
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@Slf4j
 class ElasticsearchTestApplicationTests {
 
+    private final NewElasticsearchTemplate newElasticsearchTemplate;
+
     @Test
-    void contextLoads() {
+    void isIndexExists() {
+        String indexName = "laokou_trace_202305";
+        List<String> indexNames = List.of(indexName);
+        log.info("索引是否存在：{}",newElasticsearchTemplate.isIndexExists(indexNames));
+        log.info("索引是否存在：{}",newElasticsearchTemplate.isIndexExists(indexName));
     }
 
 }
