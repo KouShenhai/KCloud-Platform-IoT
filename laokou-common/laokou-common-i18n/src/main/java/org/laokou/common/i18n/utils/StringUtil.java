332092898d0b2344349c939db72f758b446f1ca5
Administrator <2413176044@qq.com>
2023-05-18 18:04:09 +0800

    reactor：提交代码

--- laokou-common/laokou-common-i18n/src/main/java/org/laokou/common/i18n/utils/StringUtil.java
+++ laokou-common/laokou-common-i18n/src/main/java/org/laokou/common/i18n/utils/StringUtil.java
@@ -23,6 +23,8 @@ import org.springframework.lang.Nullable;
  */
 public class StringUtil {
 
+    private static final char SEPARATOR = '_';
+
     public static boolean isNotEmpty(String str) {
         return StringUtils.isNotEmpty(str);
     }
@@ -51,8 +53,30 @@ public class StringUtil {
                 return true;
             }
         }
-
         return false;
+    }
+
+    public static String toCamelCase(String s) {
+        if (s == null) {
+            return null;
+        }
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder(s.length());
+        boolean upperCase = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == SEPARATOR) {
+                upperCase = true;
+            }
+            else if (upperCase) {
+                sb.append(Character.toUpperCase(c));
+                upperCase = false;
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 
 }
---
+++ laokou-service/laokou-generate/src/main/java/org/laokou/generate/mapper/ColumnMapper.java
@@ -0,0 +1,16 @@
+package org.laokou.generate.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.laokou.generate.vo.ColumnVO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface ColumnMapper {
+
+    List<ColumnVO> getColumns(@Param("tableName")String tableName);
+
+}
