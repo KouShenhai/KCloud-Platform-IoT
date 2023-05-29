2aa8b54b492fb32d7da78c957b4dc9f3ffa26f53
koushenhai <2413176044@qq.com>
2023-05-29 08:22:13 +0800

    style: 优化代码

--- laokou-service/laokou-module/laokou-test/laokou-test-nio/src/main/java/org/laokou/test/nio/ConnectionPerThreadWithPool.java
+++ laokou-service/laokou-module/laokou-test/laokou-test-nio/src/main/java/org/laokou/test/nio/ConnectionPerThreadWithPool.java
@@ -28,13 +28,9 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author laokou
  */
-public class ConnectionPerThreadWithPool implements Runnable {
+public class ConnectionPerThreadWithPool {
 
-    public static void main(String[] args) {
-        EXECUTOR_SERVICE.execute(ConnectionPerThreadWithPool::new);
-    }
-
-    private static final ExecutorService EXECUTOR_SERVICE = new ThreadPoolExecutor(8
+    public static final ExecutorService EXECUTOR_SERVICE = new ThreadPoolExecutor(8
             ,16
             ,60
             , TimeUnit.SECONDS
@@ -42,49 +38,34 @@ public class ConnectionPerThreadWithPool implements Runnable {
             ,new ThreadPoolExecutor.AbortPolicy());
     private static final int PORT = 8088;
 
-    @Override
+    public static void main(String[] args) {
+        EXECUTOR_SERVICE.execute(() -> {
+            // 服务器监听Socket
+            try {
+                ServerSocket serverSocket = new ServerSocket(PORT);
+                // 主线程死循环，等待新连接到来
+                while (!Thread.interrupted()) {
+                    Socket socket = serverSocket.accept();
+                    // 接收一个连接后，为Socket连接新建一个专属的处理器对象
+                    EXECUTOR_SERVICE.execute(() -> {
+                        // 死循环读写事件
+                        boolean ioCompleted = false;
+                        while (!ioCompleted) {
+                            try {
+                                byte[] input = new byte[1024];
+                                socket.getInputStream().read(input);
+                                ioCompleted = true;
+                                System.out.println("读取到的结果：" + new String(input, StandardCharsets.UTF_8));
+                                socket.close();
+                            } catch (IOException ignored){
 
-    public void run() {
-        // 服务器监听Socket
-        try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            // 主线程死循环，等待新连接到来
-            while (!Thread.interrupted()) {
-                Socket socket = serverSocket.accept();
-                // 接收一个连接后，为Socket连接新建一个专属的处理器对象
-                EXECUTOR_SERVICE.execute(() -> new Handler(socket));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static class Handler implements Runnable {
-
-        private final Socket socket;
-        Handler(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            // 死循环读写事件
-            boolean ioCompleted = false;
-            while (!ioCompleted) {
-                try {
-                    byte[] input = new byte[1024];
-                    socket.getInputStream().read(input);
-                    ioCompleted = true;
-                    System.out.println("读取到的结果：" + new String(input, StandardCharsets.UTF_8));
-                } catch (IOException ignored){
-
-                } finally {
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                            }
+                        }
+                    });
                 }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        }
+        });
     }
 }
