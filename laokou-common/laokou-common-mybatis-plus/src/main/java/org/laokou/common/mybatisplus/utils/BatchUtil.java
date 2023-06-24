5fa17d6a2fa639e38760789bb3509f8ffa759947
koushenhai <2413176044@qq.com>
2023-06-24 21:59:48 +0800

    feat：优化代码

--- laokou-common/laokou-common-mybatis-plus/src/main/java/org/laokou/common/mybatisplus/utils/BatchUtil.java
+++ laokou-common/laokou-common-mybatis-plus/src/main/java/org/laokou/common/mybatisplus/utils/BatchUtil.java
@@ -19,10 +19,12 @@ import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.laokou.common.i18n.core.CustomException;
 import org.laokou.common.mybatisplus.service.BatchService;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -50,21 +52,31 @@ public class BatchUtil {
 		// 数据分组
 		List<List<T>> partition = Lists.partition(dataList, batchNum);
 		AtomicBoolean rollback = new AtomicBoolean(false);
-		partition.forEach(item -> CompletableFuture.runAsync(() -> transactionalUtil.executeWithoutResult(callback -> {
-			try {
-				service.insertBatch(item);
-			}
-			catch (Exception e) {
-				// 回滚标识
-				rollback.compareAndSet(false, true);
-				log.error("批量插入数据异常，已设置回滚标识，错误信息：{}", e.getMessage());
-			}
-			finally {
-				if (rollback.get()) {
-					callback.setRollbackOnly();
+		List<CompletableFuture<Void>> futures = new ArrayList<>(partition.size());
+		// 数据库隔离级别设置为READ-COMMITTED => 读已提交
+		// set global transaction isolation level read committed; => 全局隔离级别
+		// set session transaction isolation level read committed; => 会话隔离级别
+		// select @@global.transaction_isolation,@@transaction_isolation 或  show variables like 'transaction_isolation'
+		partition.forEach(item -> {
+			futures.add(CompletableFuture.runAsync(() -> transactionalUtil.executeWithoutResult(callback -> {
+				try {
+					service.insertBatch(item);
+				} catch (Exception e) {
+					// 回滚标识
+					rollback.compareAndSet(false,true);
+					log.error("批量插入数据异常，已设置回滚标识，错误信息：{}",e.getMessage());
+				} finally {
+					if (rollback.get()) {
+						callback.setRollbackOnly();
+					}
 				}
-			}
-		}), taskExecutor));
+			}), taskExecutor));
+		});
+		// 阻塞主线程
+		CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+		if (rollback.get()) {
+			throw new CustomException("批量插入数据异常，数据已回滚");
+		}
 	}
 
 }
