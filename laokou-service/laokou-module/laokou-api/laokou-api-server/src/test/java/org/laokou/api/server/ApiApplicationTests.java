6b63fff9715b9c65a227c1062877db6993015be2
koushenhai <2413176044@qq.com>
2023-07-02 12:30:38 +0800

    feat：增加test公共模块

--- laokou-service/laokou-module/laokou-api/laokou-api-server/src/test/java/org/laokou/api/server/ApiApplicationTests.java
+++ laokou-service/laokou-module/laokou-api/laokou-api-server/src/test/java/org/laokou/api/server/ApiApplicationTests.java
@@ -1,13 +1,21 @@
 package org.laokou.api.server;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class ApiApplicationTests {
 
+	private final MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testGet() {
+		System.out.println(mockMvc);
 	}
 
 }
