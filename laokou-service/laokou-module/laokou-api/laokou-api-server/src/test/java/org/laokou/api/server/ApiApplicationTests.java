15997850ca6b4c41e033961c1f1418d2149c5545
koushenhai <2413176044@qq.com>
2023-07-02 12:53:10 +0800

    feat：api验签

--- laokou-service/laokou-module/laokou-api/laokou-api-server/src/test/java/org/laokou/api/server/ApiApplicationTests.java
+++ laokou-service/laokou-module/laokou-api/laokou-api-server/src/test/java/org/laokou/api/server/ApiApplicationTests.java
@@ -2,11 +2,22 @@ package org.laokou.api.server;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static org.laokou.common.core.constant.Constant.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @RequiredArgsConstructor
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class ApiApplicationTests {
@@ -14,8 +25,25 @@ class ApiApplicationTests {
 	private final MockMvc mockMvc;
 
 	@Test
-	void testGet() {
-		System.out.println(mockMvc);
+	void testGet() throws Exception {
+		String appKey = "laokou2023";
+		String appSecret = "vb05f6c45d67340zaz95v7fa6d49v99zx";
+		long timestamp = System.currentTimeMillis();
+		String nonce = UUID.randomUUID().toString();
+		mockMvc.perform(get("/get")
+				.header(APP_KEY,appKey)
+				.header(APP_SECRET,appSecret)
+				.header(TIMESTAMP,String.valueOf(timestamp))
+				.header(NONCE, nonce)
+				.header(SIGN,sign(appKey,appSecret,nonce,timestamp,"")))
+				.andExpect(status().isOk())
+				.andDo(print())
+				.andReturn();
+	}
+
+	private static String sign(String appKey, String appSecret, String nonce, long timestamp, String params) {
+		String str = appKey + appSecret + nonce + timestamp + params;
+		return DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
 	}
 
 }
