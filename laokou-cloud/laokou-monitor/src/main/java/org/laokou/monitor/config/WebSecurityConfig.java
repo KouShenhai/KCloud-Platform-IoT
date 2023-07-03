16afd69632ececeaafaa158f385973114230974b
koushenhai <2413176044@qq.com>
2023-07-04 01:25:39 +0800

    feat：monitor关闭ssl校验

--- laokou-cloud/laokou-monitor/src/main/java/org/laokou/monitor/config/WebSecurityConfig.java
+++ laokou-cloud/laokou-monitor/src/main/java/org/laokou/monitor/config/WebSecurityConfig.java
@@ -20,14 +20,28 @@ package org.laokou.monitor.config;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 /**
  * @author laokou
@@ -37,6 +51,19 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 public class WebSecurityConfig {
 
 	private final AdminServerProperties adminServerProperties;
+	private final RestTemplateBuilder restTemplateBuilder;
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+		disableSsl(httpClientBuilder);
+		CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+				closeableHttpClient);
+		restTemplate.setRequestFactory(requestFactory);
+		return restTemplate;
+	}
 
 	@Bean
 	@ConditionalOnMissingBean(SecurityFilterChain.class)
@@ -57,6 +84,47 @@ public class WebSecurityConfig {
 				.logout(logout -> logout.logoutUrl(adminServerProperties.path("/logout")))
 				.httpBasic(Customizer.withDefaults())
 				.csrf(AbstractHttpConfigurer::disable).build();
+	}
+
+	static class DisableValidationTrustManager implements X509TrustManager {
+
+		DisableValidationTrustManager() {
+		}
+
+		public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
+		}
+
+		public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
+		}
+
+		public X509Certificate[] getAcceptedIssuers() {
+			return new X509Certificate[0];
+		}
+
+	}
+
+	static class TrustAllHostnames implements HostnameVerifier {
+
+		TrustAllHostnames() {
+		}
+
+		public boolean verify(String s, SSLSession sslSession) {
+			return true;
+		}
+
+	}
+
+	@SneakyThrows
+	public static void disableSsl(HttpClientBuilder builder) {
+		X509TrustManager disabledTrustManager = new DisableValidationTrustManager();
+		TrustManager[] trustManagers = new TrustManager[] { disabledTrustManager };
+		SSLContext sslContext = SSLContext.getInstance("SSL");
+		sslContext.init(null, trustManagers, new SecureRandom());
+		SSLConnectionSocketFactory sslConnectionSocketFactory = SSLConnectionSocketFactoryBuilder.create()
+				.setSslContext(sslContext).setHostnameVerifier(new TrustAllHostnames()).build();
+		PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder
+				.create().setSSLSocketFactory(sslConnectionSocketFactory).build();
+		builder.setConnectionManager(poolingHttpClientConnectionManager);
 	}
 
 }
