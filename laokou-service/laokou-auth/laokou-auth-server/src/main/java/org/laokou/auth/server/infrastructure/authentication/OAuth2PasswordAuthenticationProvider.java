d362b32d80d969321bd6c7af986870534330e149
koushenhai <2413176044@qq.com>
2023-07-24 00:24:10 +0800

    feat: auth设备码认证

--- laokou-service/laokou-auth/laokou-auth-server/src/main/java/org/laokou/auth/server/infrastructure/authentication/OAuth2PasswordAuthenticationProvider.java
+++ laokou-service/laokou-auth/laokou-auth-server/src/main/java/org/laokou/auth/server/infrastructure/authentication/OAuth2PasswordAuthenticationProvider.java
@@ -20,11 +20,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.laokou.auth.client.constant.AuthConstant;
 import org.laokou.auth.client.handler.CustomAuthExceptionHandler;
-import org.laokou.auth.server.domain.sys.repository.service.*;
-import org.laokou.common.i18n.utils.StringUtil;
+import org.laokou.auth.server.domain.sys.repository.service.SysDeptService;
+import org.laokou.auth.server.domain.sys.repository.service.SysMenuService;
+import org.laokou.auth.server.domain.sys.repository.service.SysUserService;
 import org.laokou.common.easy.captcha.service.SysCaptchaService;
 import org.laokou.common.i18n.core.StatusCode;
 import org.laokou.common.i18n.utils.MessageUtil;
+import org.laokou.common.i18n.utils.StringUtil;
 import org.laokou.common.log.utils.LoginLogUtil;
 import org.laokou.common.redis.utils.RedisUtil;
 import org.laokou.common.tenant.service.SysSourceService;
@@ -36,8 +38,6 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 /**
  * @author laokou
@@ -63,7 +63,7 @@ public class OAuth2PasswordAuthenticationProvider extends AbstractOAuth2BaseAuth
 	}
 
 	@Override
-	Authentication login(HttpServletRequest request) throws IOException {
+	Authentication login(HttpServletRequest request) {
 		// 判断唯一标识是否为空
 		String uuid = request.getParameter(AuthConstant.UUID);
 		log.info("唯一标识：{}", uuid);
