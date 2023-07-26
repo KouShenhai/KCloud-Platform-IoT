d242b017c2bebd722ba9f68ed804a6d58f766bad
Administrator <2413176044@qq.com>
2023-07-26 21:28:46 +0800

    feat:logstash增加ssl

--- laokou-service/laokou-module/laokou-logstash/laokou-logstash-rocketmq/src/main/java/org/laokou/logstash/rocketmq/RocketmqApplication.java
+++ laokou-service/laokou-module/laokou-logstash/laokou-logstash-rocketmq/src/main/java/org/laokou/logstash/rocketmq/RocketmqApplication.java
@@ -20,6 +20,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -28,6 +29,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableScheduling
+@EnableDiscoveryClient
 public class RocketmqApplication {
 
 	public static void main(String[] args) {
