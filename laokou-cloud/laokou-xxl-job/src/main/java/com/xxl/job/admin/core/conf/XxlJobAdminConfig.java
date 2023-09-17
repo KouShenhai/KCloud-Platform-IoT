bd2ef6a0888a40940dc46ab08b81c656db650e2f
koushenhai <2413176044@qq.com>
2023-09-17 13:29:21 +0800

    fix: 3.x xxl-job报错问题

--- laokou-cloud/laokou-xxl-job/src/main/java/com/xxl/job/admin/core/conf/XxlJobAdminConfig.java
+++ laokou-cloud/laokou-xxl-job/src/main/java/com/xxl/job/admin/core/conf/XxlJobAdminConfig.java
@@ -55,7 +55,7 @@ public class XxlJobAdminConfig implements InitializingBean, DisposableBean {
 	@Value("${xxl.job.i18n}")
 	private String i18n;
 
-	@Value("${xxl.job.accessToken}")
+	@Value("${xxl.job.access-token}")
 	private String accessToken;
 
 	@Value("${spring.mail.from}")
