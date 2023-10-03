476ac9f93aea51817447b6e5adb844601169d008
koushenhai <2413176044@qq.com>
2023-10-03 13:56:13 +0800

    feat: 优化sql

--- laokou-service/laokou-admin/laokou-admin-infrastructure/src/main/java/org/laokou/admin/gatewayimpl/UserGatewayImpl.java
+++ laokou-service/laokou-admin/laokou-admin-infrastructure/src/main/java/org/laokou/admin/gatewayimpl/UserGatewayImpl.java
@@ -151,7 +151,7 @@ public class UserGatewayImpl implements UserGateway {
 
 	@Transactional(rollbackFor = Exception.class)
 	public Boolean updateUser(UserDO userDO, User user, List<Long> ids) {
-		boolean flag = userMapper.updateUser(userDO) > 0;
+		boolean flag = userMapper.updateUser(userDO,TableUtil.getUserTable(userDO.getId())) > 0;
 		flag = flag && deleteUserRole(ids);
 		return flag && insertUserRole(userDO.getId(), user.getRoleIds(), user);
 	}
@@ -205,7 +205,7 @@ public class UserGatewayImpl implements UserGateway {
 
 	@Transactional(rollbackFor = Exception.class)
 	public Boolean updateUser(UserDO userDO) {
-		return userMapper.updateUser(userDO) > 0;
+		return userMapper.updateUser(userDO,TableUtil.getUserTable(userDO.getId())) > 0;
 	}
 
 }
