72bae412276185aa3a49743e3418f5b928f4a465
koushenhai <2413176044@qq.com>
2023-10-26 18:54:00 +0800

    feat：增加kafka代码

--- laokou-common/laokou-common-kafka/src/main/java/org/laokou/common/kafka/constant/MqConstant.java
+++ laokou-common/laokou-common-kafka/src/main/java/org/laokou/common/kafka/constant/MqConstant.java
@@ -1,38 +1,27 @@
 /*
- * Copyright (c) 2022 KCloud-Platform-Alibaba Author or Authors. All Rights Reserved.
- *
+ * Copyright (c) 2022 KCloud-Platform-Alibaba Authors. All Rights Reserved.
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-package org.laokou.common.rocketmq.constant;
+package org.laokou.common.kafka.constant;
 
 /**
- * 队列常量值
- *
  * @author laokou
  */
 public interface MqConstant {
 
-	String LAOKOU_MESSAGE_TOPIC = "laokou_message_topic";
+    String LAOKOU_TRACE_TOPIC = "laokou_trace_topic";
 
-	String LAOKOU_NOTICE_MESSAGE_TAG = "notice";
-
-	String LAOKOU_REMIND_MESSAGE_TAG = "remind";
-
-	String LAOKOU_REMIND_MESSAGE_CONSUMER_GROUP = "laokou_remind_message_consumer_group";
-
-	String LAOKOU_NOTICE_MESSAGE_CONSUMER_GROUP = "laokou_notice_message_consumer_group";
-
-	String TOPIC_TAG = "%s:%s";
+    String LAOKOU_LOGSTASH_CONSUMER_GROUP = "laokou_logstash_consumer_group";
 
 }
---
+++ laokou-service/laokou-module/laokou-logstash/laokou-logstash-app/src/main/java/org/laokou/logstash/consumer/TraceConsumer.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022 KCloud-Platform-Alibaba Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.laokou.logstash.consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.laokou.common.core.utils.JacksonUtil;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.laokou.common.kafka.constant.MqConstant.LAOKOU_LOGSTASH_CONSUMER_GROUP;
+import static org.laokou.common.kafka.constant.MqConstant.LAOKOU_TRACE_TOPIC;
+
+/**
+ * @author laokou
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TraceConsumer {
+
+    @KafkaListener(topics = LAOKOU_TRACE_TOPIC, groupId = LAOKOU_LOGSTASH_CONSUMER_GROUP)
+    public void kafkaConsumerTest(List<String> message, Acknowledgment ack) {
+        log.info("接收到消息：{}", JacksonUtil.toJsonStr(message));
+        ack.acknowledge();
+    }
+
+}
