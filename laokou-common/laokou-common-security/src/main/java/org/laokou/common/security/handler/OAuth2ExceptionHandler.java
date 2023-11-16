a4f8c1a2bf5f1db93015afcd8d61b14eeef7b80c
koushenhai <2413176044@qq.com>
2023-11-16 18:21:49 +0800

    feat: h优化代码

--- laokou-common/laokou-common-security/src/main/java/org/laokou/common/security/handler/OAuth2ExceptionHandler.java
+++ laokou-common/laokou-common-security/src/main/java/org/laokou/common/security/handler/OAuth2ExceptionHandler.java
@@ -1,5 +1,13 @@
 package org.laokou.common.security.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.laokou.common.core.utils.ResponseUtil;
+import org.laokou.common.i18n.common.StatusCode;
+import org.laokou.common.i18n.utils.MessageUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2Error;
 
@@ -16,6 +24,23 @@ public class OAuth2ExceptionHandler {
 
 	public static OAuth2AuthenticationException getException(int errorCode, String description) {
 		return getException(String.valueOf(errorCode), description, ERROR_URI);
+	}
+
+	@SneakyThrows
+	public static void handle(HttpServletRequest request, HttpServletResponse response, Throwable ex) {
+		if (ex instanceof InsufficientAuthenticationException) {
+			ResponseUtil.response(response, StatusCode.UNAUTHORIZED, MessageUtil.getMessage(StatusCode.UNAUTHORIZED));
+			return;
+		}
+		if (ex instanceof AccessDeniedException) {
+			ResponseUtil.response(response, StatusCode.FORBIDDEN, MessageUtil.getMessage(StatusCode.FORBIDDEN));
+			return;
+		}
+		if (ex instanceof OAuth2AuthenticationException authenticationException) {
+			String message = authenticationException.getError().getDescription();
+			int errorCode = Integer.parseInt(authenticationException.getError().getErrorCode());
+			ResponseUtil.response(response, errorCode, message);
+        }
 	}
 
 }
