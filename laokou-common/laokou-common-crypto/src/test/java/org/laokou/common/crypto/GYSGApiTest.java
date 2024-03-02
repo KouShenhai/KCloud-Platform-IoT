d4ab51fc09b221ef68e418495ec711147a0497ba
koushenhai <2413176044@qq.com>
2024-03-02 12:51:11 +0800

    feat：spring-cloud-alibaba升级至2023

--- laokou-common/laokou-common-crypto/src/test/java/org/laokou/common/crypto/GYSGApiTest.java
+++ laokou-common/laokou-common-crypto/src/test/java/org/laokou/common/crypto/GYSGApiTest.java
@@ -29,75 +29,98 @@ import java.util.HashMap;
  */
 @Slf4j
 public class GYSGApiTest {
-    public static void main(String[] args) {
-        HashMap<String, String> params = new HashMap<>(2);
-        String key = "";
-        String cx = "";
-        params.put("key", key);
-        params.put("cx", cx);
-        String result = HttpUtil.doGet("https://api.t1qq.com/api/sky/sc/sg", params, new HashMap<>(0), true);
-        Result res = JacksonUtil.toBean(result, Result.class);
-        Result.Action action = res.action;
-        Result.Adorn adorn = res.adorn;
-        Result.Data data = res.data;
-        String str = """
-                \n亲爱的光崽，你好！
-                -----基本信息-----
-                当前身高：{}
-                最大身高：{}
-                最小身高：{}
-                体型值：{}
-                身高：{}
-                -----装扮信息-----
-                斗篷：{}
-                道具：{}
-                脖子：{}
-                面具：{}
-                耳朵：{}
-                头发：{}
-                裤子：{}
-                -----动作信息-----
-                声音：{}
-                站姿：{}
-                """;
-        log.info(str, data.currentHeight, data.maxHeight, data.minHeight, data.scale, data.height, adorn.cloak,adorn.prop,adorn.neck,adorn.mask,adorn.horn,adorn.hair,adorn.pants,action.voice,action.attitude);
-    }
 
-    @Data
-    static class Result {
+	public static void main(String[] args) {
+		HashMap<String, String> params = new HashMap<>(2);
+		String key = "";
+		String cx = "";
+		params.put("key", key);
+		params.put("cx", cx);
+		String result = HttpUtil.doGet("https://api.t1qq.com/api/sky/sc/sg", params, new HashMap<>(0), true);
+		Result res = JacksonUtil.toBean(result, Result.class);
+		Result.Action action = res.action;
+		Result.Adorn adorn = res.adorn;
+		Result.Data data = res.data;
+		String str = """
+				\n亲爱的光崽，你好！
+				-----基本信息-----
+				当前身高：{}
+				最大身高：{}
+				最小身高：{}
+				体型值：{}
+				身高：{}
+				-----装扮信息-----
+				斗篷：{}
+				道具：{}
+				脖子：{}
+				面具：{}
+				耳朵：{}
+				头发：{}
+				裤子：{}
+				-----动作信息-----
+				声音：{}
+				站姿：{}
+				""";
+		log.info(str, data.currentHeight, data.maxHeight, data.minHeight, data.scale, data.height, adorn.cloak,
+				adorn.prop, adorn.neck, adorn.mask, adorn.horn, adorn.hair, adorn.pants, action.voice, action.attitude);
+	}
 
-        private Integer code;
-        private String time;
-        private Data data;
-        private Adorn adorn;
-        private Action action;
+	@Data
+	static class Result {
 
-        @lombok.Data
-        static class Data {
-            private String scale;
-            private String height;
-            private String currentHeight;
-            private String maxHeight;
-            private String minHeight;
-        }
+		private Integer code;
 
-        @lombok.Data
-        static class Adorn {
-            private String cloak;
-            private String prop;
-            private String neck;
-            private String mask;
-            private String horn;
-            private String hair;
-            private String pants;
-        }
+		private String time;
 
-        @lombok.Data
-        static class Action {
-            private String voice;
-            private String attitude;
-        }
+		private Data data;
 
-    }
+		private Adorn adorn;
+
+		private Action action;
+
+		@lombok.Data
+		static class Data {
+
+			private String scale;
+
+			private String height;
+
+			private String currentHeight;
+
+			private String maxHeight;
+
+			private String minHeight;
+
+		}
+
+		@lombok.Data
+		static class Adorn {
+
+			private String cloak;
+
+			private String prop;
+
+			private String neck;
+
+			private String mask;
+
+			private String horn;
+
+			private String hair;
+
+			private String pants;
+
+		}
+
+		@lombok.Data
+		static class Action {
+
+			private String voice;
+
+			private String attitude;
+
+		}
+
+	}
 
 }
