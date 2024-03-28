32052f07ae154003d73c11eb7b85d5eecfba9324
koushenhai <2413176044@qq.com>
2024-03-28 22:44:08 +0800

    style: 优化代码

---
+++ laokou-go-test/Test5.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s = "333"
+	fmt.Println(s)
+	var b, c = 1, 2
+	var dd int
+	var ee bool
+	var x, y int
+	var (
+		xx int
+		yy int
+	)
+	g, h := 1, 2
+	fmt.Println(b, c)
+	fmt.Println(dd, ee)
+	fmt.Println(x, y, xx, yy)
+	fmt.Println(g, h)
+}
