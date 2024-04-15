bf1b43e4cb71c0eeeadf262fe970064850078002
koushenhai <2413176044@qq.com>
2024-04-16 00:22:42 +0800

    feat: vue2

--- laokou-test/laokou-test-container/laokou-test-container-base/src/main/java/org/laokou/test/container/GinCrudApiGo.java
+++ laokou-test/laokou-test-container/laokou-test-container-base/src/main/java/org/laokou/test/container/GinCrudApiGo.java
@@ -1,161 +1,179 @@
+/*
+ * Copyright (c) 2022-2024 KCloud-Platform-Alibaba Author or Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.laokou.test.container;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.laokou.common.core.utils.TemplateUtil;
 
 import java.util.Map;
 
-public class GinCrudApiGo implements Crud{
+@Slf4j
+public class GinCrudApiGo implements Crud {
+
 	public static void main(String[] args) {
 		String inst = "out";
 		String upper = "Output";
 		String lower = "output";
-		Map<String, Object> params = Map.of("inst",inst, "upper",upper, "lower",lower);
+		Map<String, Object> params = Map.of("inst", inst, "upper", upper, "lower", lower);
 		Crud crud = new GinCrudApiGo();
-		System.out.println();
-		System.out.println(crud.imp(params));
-		System.out.println(crud.type(params));
-		System.out.println(crud.create(params));
-		System.out.println(crud.remove(params));
-		System.out.println(crud.modify(params));
-		System.out.println(crud.find(params));
-		System.out.println();
+		log.info(crud.imp(params));
+		log.info(crud.type(params));
+		log.info(crud.create(params));
+		log.info(crud.remove(params));
+		log.info(crud.modify(params));
+		log.info(crud.find(params));
 	}
 
 	@SneakyThrows
 	public String imp(Map<String, Object> params) {
 		String str = """
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"go.uber.org/zap"
-	"lc-base-frame/model/common/request"
-	"lc-base-frame/model/common/response"
-	"lc-base-frame/model/system"
-	"lc-base-frame/utils"
-)
-			""";
+				import (
+					"github.com/gin-gonic/gin"
+					"github.com/sirupsen/logrus"
+					"go.uber.org/zap"
+					"lc-base-frame/model/common/request"
+					"lc-base-frame/model/common/response"
+					"lc-base-frame/model/system"
+					"lc-base-frame/utils"
+				)
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
 	@SneakyThrows
 	public String type(Map<String, Object> params) {
 		String str = """
-type AtSys${upper}Api struct{}
-			""";
+				type AtSys${upper}Api struct{}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
 	@SneakyThrows
 	public String find(Map<String, Object> params) {
 		String str = """
-func (api *AtSys${upper}Api) Find${upper}List(c *gin.Context) {
-	var pageInfo request.PageInfo
-	err := c.ShouldBindJSON(&pageInfo)
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = utils.Verify(pageInfo, utils.PageInfoVerify)
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	list, total, err := ${lower}Service.Find${upper}List(pageInfo)
-	if err != nil {
-		logrus.Error("获取失败!", err)
-		response.FailWithMessage("获取失败", c)
-		return
-	}
-	response.OkWithDetailed(response.PageResult{
-		List:     list,
-		Total:    total,
-		Page:     pageInfo.Page,
-		PageSize: pageInfo.PageSize,
-	}, "获取成功", c)
-}
-			""";
+				func (api *AtSys${upper}Api) Find${upper}List(c *gin.Context) {
+					var pageInfo request.PageInfo
+					err := c.ShouldBindJSON(&pageInfo)
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = utils.Verify(pageInfo, utils.PageInfoVerify)
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					list, total, err := ${lower}Service.Find${upper}List(pageInfo)
+					if err != nil {
+						logrus.Error("获取失败!", err)
+						response.FailWithMessage("获取失败", c)
+						return
+					}
+					response.OkWithDetailed(response.PageResult{
+						List:     list,
+						Total:    total,
+						Page:     pageInfo.Page,
+						PageSize: pageInfo.PageSize,
+					}, "获取成功", c)
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
 	@SneakyThrows
 	public String create(Map<String, Object> params) {
 		String str = """
-func (api *AtSys${upper}Api) Create${upper}(c *gin.Context) {
-	var ${inst} system.AtSys${upper}
-	err := c.ShouldBindJSON(&${inst})
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = utils.Verify(${inst}, utils.${upper}Verify)
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = ${lower}Service.Create${upper}(${inst})
-	if err != nil {
-		logrus.Error("添加失败!", zap.Error(err))
-		response.FailWithMessage("添加失败", c)
-		return
-	}
-	response.OkWithMessage("添加成功", c)
-}
-			""";
+				func (api *AtSys${upper}Api) Create${upper}(c *gin.Context) {
+					var ${inst} system.AtSys${upper}
+					err := c.ShouldBindJSON(&${inst})
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = utils.Verify(${inst}, utils.${upper}Verify)
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = ${lower}Service.Create${upper}(${inst})
+					if err != nil {
+						logrus.Error("添加失败!", zap.Error(err))
+						response.FailWithMessage("添加失败", c)
+						return
+					}
+					response.OkWithMessage("添加成功", c)
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
 	@SneakyThrows
 	public String modify(Map<String, Object> params) {
 		String str = """
-func (api *AtSys${upper}Api) Modify${upper}(c *gin.Context) {
-	var ${inst} system.AtSys${upper}
-	err := c.ShouldBindJSON(&${inst})
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = utils.Verify(${inst}, utils.${upper}Verify)
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = ${lower}Service.Modify${upper}(${inst})
-	if err != nil {
-		logrus.Error("更新失败!", zap.Error(err))
-		response.FailWithMessage("更新失败", c)
-		return
-	}
-	response.OkWithMessage("更新成功", c)
-}
-			""";
+				func (api *AtSys${upper}Api) Modify${upper}(c *gin.Context) {
+					var ${inst} system.AtSys${upper}
+					err := c.ShouldBindJSON(&${inst})
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = utils.Verify(${inst}, utils.${upper}Verify)
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = ${lower}Service.Modify${upper}(${inst})
+					if err != nil {
+						logrus.Error("更新失败!", zap.Error(err))
+						response.FailWithMessage("更新失败", c)
+						return
+					}
+					response.OkWithMessage("更新成功", c)
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
 	@SneakyThrows
 	public String remove(Map<String, Object> params) {
 		String str = """
-func (api *AtSys${upper}Api) Remove${upper}(c *gin.Context) {
-	var ${inst} request.GetById
-	err := c.ShouldBindJSON(&${inst})
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = utils.Verify(${inst}, utils.IdVerify)
-	if err != nil {
-		response.FailWithMessage(err.Error(), c)
-		return
-	}
-	err = ${lower}Service.Remove${upper}(${inst}.ID)
-	if err != nil {
-		logrus.Error("删除失败!", zap.Error(err))
-		response.FailWithMessage("删除失败", c)
-		return
-	}
-	response.OkWithMessage("删除成功", c)
-}
-			""";
+				func (api *AtSys${upper}Api) Remove${upper}(c *gin.Context) {
+					var ${inst} request.GetById
+					err := c.ShouldBindJSON(&${inst})
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = utils.Verify(${inst}, utils.IdVerify)
+					if err != nil {
+						response.FailWithMessage(err.Error(), c)
+						return
+					}
+					err = ${lower}Service.Remove${upper}(${inst}.ID)
+					if err != nil {
+						logrus.Error("删除失败!", zap.Error(err))
+						response.FailWithMessage("删除失败", c)
+						return
+					}
+					response.OkWithMessage("删除成功", c)
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
--- laokou-test/laokou-test-container/laokou-test-container-base/src/main/java/org/laokou/test/container/GinCrudServiceGo.java
+++ laokou-test/laokou-test-container/laokou-test-container-base/src/main/java/org/laokou/test/container/GinCrudServiceGo.java
@@ -1,37 +1,55 @@
+/*
+ * Copyright (c) 2022-2024 KCloud-Platform-Alibaba Author or Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.laokou.test.container;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.laokou.common.core.utils.TemplateUtil;
 
 import java.util.Map;
 
-public class GinCrudServiceGo implements Crud{
+@Slf4j
+public class GinCrudServiceGo implements Crud {
+
 	public static void main(String[] args) {
 		String inst = "pro";
 		String upper = "Protocol";
 		String lower = "protocol";
-		Map<String, Object> params = Map.of("inst",inst, "upper",upper, "lower",lower);
+		Map<String, Object> params = Map.of("inst", inst, "upper", upper, "lower", lower);
 		Crud crud = new GinCrudServiceGo();
-		System.out.println();
-		System.out.println(crud.imp(params));
-		System.out.println(crud.type(params));
-		System.out.println(crud.create(params));
-		System.out.println(crud.remove(params));
-		System.out.println(crud.modify(params));
-		System.out.println(crud.find(params));
-		System.out.println();
+		log.info(crud.imp(params));
+		log.info(crud.type(params));
+		log.info(crud.create(params));
+		log.info(crud.remove(params));
+		log.info(crud.modify(params));
+		log.info(crud.find(params));
 	}
 
 	@Override
 	@SneakyThrows
 	public String create(Map<String, Object> params) {
 		String str = """
-func (service *${upper}Service) Create${upper}(${inst} system.AtSys${upper}) error {
-	${inst}.CreateDate = time.Now()
-	${inst}.UpdateDate = time.Now()
-	return global.Db.Create(&${inst}).Error
-}
-			""";
+				func (service *${upper}Service) Create${upper}(${inst} system.AtSys${upper}) error {
+					${inst}.CreateDate = time.Now()
+					${inst}.UpdateDate = time.Now()
+					return global.Db.Create(&${inst}).Error
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
@@ -39,11 +57,11 @@ func (service *${upper}Service) Create${upper}(${inst} system.AtSys${upper}) err
 	@SneakyThrows
 	public String remove(Map<String, Object> params) {
 		String str = """
-func (service *${upper}Service) Remove${upper}(id int) (err error) {
-	var ${inst} system.AtSys${upper}
-	return global.Db.Model(&${inst}).Where("id = ?", id).Update("del_flag", "1").Error
-}
-			""";
+				func (service *${upper}Service) Remove${upper}(id int) (err error) {
+					var ${inst} system.AtSys${upper}
+					return global.Db.Model(&${inst}).Where("id = ?", id).Update("del_flag", "1").Error
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
@@ -51,11 +69,11 @@ func (service *${upper}Service) Remove${upper}(id int) (err error) {
 	@SneakyThrows
 	public String modify(Map<String, Object> params) {
 		String str = """
-func (service *${upper}Service) Modify${upper}(${inst} system.AtSys${upper}) (err error) {
-	${lower}Map := map[string]interface{}{}
-	return global.Db.Updates(${lower}Map).Error
-}
-			""";
+				func (service *${upper}Service) Modify${upper}(${inst} system.AtSys${upper}) (err error) {
+					${lower}Map := map[string]interface{}{}
+					return global.Db.Updates(${lower}Map).Error
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
@@ -63,13 +81,13 @@ func (service *${upper}Service) Modify${upper}(${inst} system.AtSys${upper}) (er
 	@SneakyThrows
 	public String imp(Map<String, Object> params) {
 		String str = """
-import (
-	"lc-base-frame/global"
-	"lc-base-frame/model/common/request"
-	"lc-base-frame/model/system"
-	"time"
-)
-			""";
+				import (
+					"lc-base-frame/global"
+					"lc-base-frame/model/common/request"
+					"lc-base-frame/model/system"
+					"time"
+				)
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
@@ -77,19 +95,19 @@ import (
 	@SneakyThrows
 	public String find(Map<String, Object> params) {
 		String str = """
-func (service *${upper}Service) Find${upper}List(info request.PageInfo) (list interface{}, total int64, err error) {
-	limit := info.PageSize
-	offset := info.PageSize * (info.Page - 1)
-	db := global.Db.Model(&system.AtSys${upper}{})
-	var ${lower}List []system.AtSys${upper}
-	err = db.Count(&total).Error
-	if err != nil {
-		return
-	}
-	err = db.Limit(limit).Offset(offset).Find(&${lower}List).Error
-	return ${lower}List, total, err
-}
-			""";
+				func (service *${upper}Service) Find${upper}List(info request.PageInfo) (list interface{}, total int64, err error) {
+					limit := info.PageSize
+					offset := info.PageSize * (info.Page - 1)
+					db := global.Db.Model(&system.AtSys${upper}{})
+					var ${lower}List []system.AtSys${upper}
+					err = db.Count(&total).Error
+					if err != nil {
+						return
+					}
+					err = db.Limit(limit).Offset(offset).Find(&${lower}List).Error
+					return ${lower}List, total, err
+				}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
 
@@ -97,8 +115,9 @@ func (service *${upper}Service) Find${upper}List(info request.PageInfo) (list in
 	@SneakyThrows
 	public String type(Map<String, Object> params) {
 		String str = """
-type ${upper}Service struct{}
-			""";
+				type ${upper}Service struct{}
+							""";
 		return TemplateUtil.getContent(str, params);
 	}
+
 }
