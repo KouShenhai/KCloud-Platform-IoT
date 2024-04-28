ad17e1adf0f55afb472febe9968b22ee55f92cb5
koushenhai <2413176044@qq.com>
2024-04-28 16:21:00 +0800

    refactor: 前端异步提交

--- laokou-ui/src/store/modules/user.js
+++ laokou-ui/src/store/modules/user.js
@@ -35,7 +35,7 @@ const user = {
 
   actions: {
     // 登录
-    Login (commit) {
+    Login ({ commit }, params) {
       return new Promise((resolve, reject) => {
         login(params).then(res => {
           storage.set(ACCESS_TOKEN, res.access_token, 60 * 60 * 1000)
