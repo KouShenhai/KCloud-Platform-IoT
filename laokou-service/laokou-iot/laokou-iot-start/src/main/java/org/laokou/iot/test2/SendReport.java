b6094a556e0dfa44717eb04490d8056c3f0d7321
koushenhai <2413176044@qq.com>
2024-05-22 19:34:59 +0800

    feat：1

--- laokou-service/laokou-iot/laokou-iot-start/src/main/java/org/laokou/iot/test2/SendReport.java
+++ laokou-service/laokou-iot/laokou-iot-start/src/main/java/org/laokou/iot/test2/SendReport.java
@@ -19,24 +19,24 @@ package org.laokou.iot.test2;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.laokou.mqtt.template.MqttTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SendReport {
 
-	// private final MqttTemplate mqttTemplate;
+	private final MqttTemplate mqttTemplate;
 
 	@PostConstruct
 	public void test() {
 		String s = """
 				{
-					"deviceId":"1792517553561415680",
+					"deviceId":"1792463076010893312",
 					"properties":{"temperature":36.8}
 				}
 				""";
-		// mqttTemplate.send("/1792515615314817024/1792517553561415680/properties/report",
-		// s);
+		mqttTemplate.send("/1789124747328585728/1792463076010893312/properties/report", s);
 	}
 
 }
