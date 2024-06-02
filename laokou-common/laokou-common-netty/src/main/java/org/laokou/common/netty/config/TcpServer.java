f9a6e3b97a98159d9dbe2c3fd67c24125880f92b
koushenhai <2413176044@qq.com>
2024-06-02 15:31:47 +0800

    docs：优化代码

--- laokou-common/laokou-common-netty/src/main/java/org/laokou/common/netty/config/TcpServer.java
+++ laokou-common/laokou-common-netty/src/main/java/org/laokou/common/netty/config/TcpServer.java
@@ -20,8 +20,6 @@ package org.laokou.common.netty.config;
 import io.netty.bootstrap.AbstractBootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.WriteBufferWaterMark;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -47,18 +45,10 @@ public class TcpServer extends AbstractServer {
 		return serverBootstrap.group(boss, work)
 			// 指定通道
 			.channel(NioServerSocketChannel.class)
-			// 开启TCP底层心跳，维持长连接
-			.childOption(ChannelOption.SO_KEEPALIVE, true)
+			// 延迟发送
+			.childOption(NioChannelOption.TCP_NODELAY, true)
 			// 请求队列最大长度（如果连接建立频繁，服务器处理创建新连接较慢，可以适当调整参数）
-			.option(ChannelOption.SO_BACKLOG, 2048)
-			// 重复使用端口
-			.option(NioChannelOption.SO_REUSEADDR, true)
-			// 接收缓冲区大小
-			.option(ChannelOption.SO_RCVBUF, 10 * 1024)
-			// 发送缓冲区大小
-			.childOption(ChannelOption.SO_SNDBUF, 10 * 1024)
-			// 设置高低水位，防止占用大量内存
-			.childOption(ChannelOption.WRITE_BUFFER_WATER_MARK, new WriteBufferWaterMark(1024, 10 * 1024))
+			.option(NioChannelOption.SO_BACKLOG, 1024)
 			// tcp处理类
 			.childHandler(channelInitializer);
 	}
