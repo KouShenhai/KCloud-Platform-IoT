e5f021b9e08acb6a1ee6812a41659437aa80f5b8
koushenhai <2413176044@qq.com>
2024-06-25 10:44:28 +0800

    feat：111

--- laokou-ui/config/routes.ts
+++ laokou-ui/config/routes.ts
@@ -1,5 +1,5 @@
 ﻿/**
- * @name umi 的路由配置
+ * umi 的路由配置
  * @description 只支持 path,component,routes,redirect,wrappers,name,icon 的配置
  * @param path  path 只支持两种占位符配置，第一种是动态参数 :id 的形式，第二种是 * 通配符，通配符只能出现路由字符串的最后。
  * @param component 配置 location 和 path 匹配后用于渲染的 React 组件路径。可以是绝对路径，也可以是相对路径，如果是相对路径，会从 src/pages 开始找起。
