26af7bb59a8c12aee3c7b6fa9ca185d9541c976b
koushenhai <2413176044@qq.com>
2024-07-05 21:46:16 +0800

    feat: 初始化

---
+++ ui/config/config.ts
@@ -0,0 +1,40 @@
+import {defineConfig} from '@umijs/max';
+
+export default defineConfig({
+	antd: {},
+	access: {},
+	model: {},
+	initialState: {},
+	request: {},
+	layout: {
+		title: '@umijs/max',
+	},
+	routes: [
+		{
+			path: '/',
+			redirect: '/home',
+		},
+		{
+			name: '首页',
+			path: '/home',
+			component: './Home',
+		},
+		{
+			name: '权限演示',
+			path: '/access',
+			component: './Access',
+		},
+		{
+			name: 'Login',
+			path: '/login',
+			component: './Login',
+			layout: false
+		},
+		{
+			name: ' CRUD 示例',
+			path: '/table',
+			component: './Table',
+		}
+	],
+	npmClient: 'pnpm',
+});
