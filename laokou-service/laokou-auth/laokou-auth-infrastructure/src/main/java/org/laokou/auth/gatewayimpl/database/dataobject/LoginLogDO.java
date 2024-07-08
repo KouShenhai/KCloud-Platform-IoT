f15853b15968d1b3d156db0fcfdb764461c476ed
koushenhai <2413176044@qq.com>
2024-07-08 12:05:11 +0800

    feat: 修改登录日志DO

--- laokou-service/laokou-auth/laokou-auth-infrastructure/src/main/java/org/laokou/auth/gatewayimpl/database/dataobject/LoginLogDO.java
+++ laokou-service/laokou-auth/laokou-auth-infrastructure/src/main/java/org/laokou/auth/gatewayimpl/database/dataobject/LoginLogDO.java
@@ -28,41 +28,61 @@ import static org.laokou.common.i18n.common.DSConstant.BOOT_SYS_LOGIN_LOG;
 import static org.laokou.common.mybatisplus.config.DynamicTableNameHandler.PLACE_HOLDER;
 
 /**
+ * 登录日志.
+ *
  * @author laokou
  */
 @Data
 @TableName(BOOT_SYS_LOGIN_LOG + PLACE_HOLDER)
-@Schema(name = "LoginLogDO", description = "登录日志")
 public class LoginLogDO extends BaseDO {
 
 	@Serial
 	private static final long serialVersionUID = 4289483981365827983L;
 
-	@Schema(name = "username", description = "登录的用户名")
+	/**
+	 * 登录的用户名.
+	 */
+	@Schema(name = "username", description = "")
 	private String username;
 
-	@Schema(name = "ip", description = "登录的IP地址")
+	/**
+	 * 登录的IP地址.
+	 */
 	private String ip;
 
-	@Schema(name = "address", description = "登录的归属地")
+	/**
+	 * 登录的归属地.
+	 */
 	private String address;
 
-	@Schema(name = "browser", description = "登录的浏览器")
+	/**
+	 * 登录的浏览器.
+	 */
 	private String browser;
 
-	@Schema(name = "os", description = "登录的操作系统")
+	/**
+	 * 登录的操作系统.
+	 */
 	private String os;
 
-	@Schema(name = "status", description = "登录状态 0登录成功 1登录失败")
+	/**
+	 * 登录状态 0登录成功 1登录失败.
+	 */
 	private Integer status;
 
-	@Schema(name = "message", description = "登录信息")
-	private String message;
+	/**
+	 * 错误信息.
+	 */
+	private String errorMessage;
 
-	@Schema(name = "type", description = "登录类型")
+	/**
+	 * 登录类型.
+	 */
 	private String type;
 
-	@Schema(name = "eventId", description = "事件ID")
+	/**
+	 * 事件ID.
+	 */
 	private Long eventId;
 
 }
