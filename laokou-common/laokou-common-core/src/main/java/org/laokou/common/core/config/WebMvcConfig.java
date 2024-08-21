356675b045fa91f173eae2b00c50269da192c2c0
koushenhai <2413176044@qq.com>
2024-08-21 18:30:12 +0800

    feat(): 增加拦截

--- laokou-common/laokou-common-core/src/main/java/org/laokou/common/core/config/WebMvcConfig.java
+++ laokou-common/laokou-common-core/src/main/java/org/laokou/common/core/config/WebMvcConfig.java
@@ -43,7 +43,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new I18nInterceptor());
+		registry.addInterceptor(new I18nInterceptor()).addPathPatterns("/**");
 	}
 
 }
