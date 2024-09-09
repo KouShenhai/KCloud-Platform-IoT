9b76675cbde70dd47bae0236e2179de14feacc94
koushenhai <2413176044@qq.com>
2024-09-09 16:09:58 +0800

    feat:  disruptor涉及复杂事件不好处理

--- laokou-common/laokou-common-domain/src/main/java/org/laokou/common/domain/config/DomainAutoConfig.java
+++ laokou-common/laokou-common-domain/src/main/java/org/laokou/common/domain/config/DomainAutoConfig.java
@@ -17,16 +17,8 @@
 
 package org.laokou.common.domain.config;
 
-import com.lmax.disruptor.RingBuffer;
-import com.lmax.disruptor.YieldingWaitStrategy;
-import com.lmax.disruptor.dsl.Disruptor;
-import com.lmax.disruptor.dsl.ProducerType;
-import org.laokou.common.domain.factory.DefaultDomainEventFactory;
-import org.laokou.common.domain.handler.DisruptorAbstractDomainEventHandler;
-import org.laokou.common.i18n.dto.DefaultExtDomainEvent;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author laokou
@@ -34,20 +26,5 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 @MapperScan("org.laokou.common.domain.mapper")
 public class DomainAutoConfig {
-
-	@Bean(destroyMethod = "shutdown", initMethod = "start")
-	public Disruptor<DefaultExtDomainEvent> sharedDisruptor(
-			DisruptorAbstractDomainEventHandler disruptorAbstractDomainEventHandler) {
-		// 创建一个可以容纳不同事件的Disruptor
-		Disruptor<DefaultExtDomainEvent> disruptor = new Disruptor<>(new DefaultDomainEventFactory(), 8096,
-				Thread.ofVirtual().factory(), ProducerType.SINGLE, new YieldingWaitStrategy());
-		disruptor.handleEventsWith(disruptorAbstractDomainEventHandler);
-		return disruptor;
-	}
-
-	@Bean
-	public RingBuffer<DefaultExtDomainEvent> ringBuffer(Disruptor<DefaultExtDomainEvent> sharedDisruptor) {
-		return sharedDisruptor.getRingBuffer();
-	}
 
 }
