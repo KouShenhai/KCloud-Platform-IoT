4a8596af22a4faff3ee2a3025309751c16755f72
koushenhai <2413176044@qq.com>
2024-11-08 13:36:13 +0800

    feat: mybatis-spring魔改源码，去掉synchronized，防止死锁

--- laokou-service/laokou-auth/laokou-auth-infrastructure/src/main/java/org/springframework/security/web/authentication/ui/DefaultLoginPageGeneratingFilter.java
+++ laokou-service/laokou-auth/laokou-auth-infrastructure/src/main/java/org/springframework/security/web/authentication/ui/DefaultLoginPageGeneratingFilter.java
@@ -45,6 +45,7 @@ import org.laokou.auth.api.TenantsServiceI;
 import org.laokou.common.core.utils.SpringContextUtil;
 import org.laokou.common.i18n.dto.Option;
 import org.laokou.common.i18n.dto.Result;
+import org.laokou.common.i18n.utils.ObjectUtil;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -102,6 +103,8 @@ public class DefaultLoginPageGeneratingFilter extends GenericFilterBean {
 	private Map<String, String> oauth2AuthenticationUrlToClientName;
 
 	private Map<String, String> saml2AuthenticationUrlToProviderName;
+
+	private TenantsServiceI tenantsServiceI;
 
 	private Function<HttpServletRequest, Map<String, String>> resolveHiddenInputs = (request) -> Collections.emptyMap();
 
@@ -162,8 +165,8 @@ public class DefaultLoginPageGeneratingFilter extends GenericFilterBean {
 	}
 
 	private String generateLoginPageHtml(HttpServletRequest request, boolean loginError, boolean logoutSuccess) {
-		TenantsServiceI serviceI = SpringContextUtil.getBean(TenantsServiceI.class);
-		Result<List<Option>> result = serviceI.listOption();
+		initTenantsServiceI();
+		Result<List<Option>> result = tenantsServiceI.listOption();
 		String errorMsg = loginError ? getLoginErrorMessage(request) : "Invalid credentials";
 		String contextPath = request.getContextPath();
 		StringBuilder sb = new StringBuilder();
@@ -315,6 +318,12 @@ public class DefaultLoginPageGeneratingFilter extends GenericFilterBean {
 			return uri.equals(url);
 		}
 		return uri.equals(request.getContextPath() + url);
+	}
+
+	private void initTenantsServiceI() {
+		if (ObjectUtil.isNull(tenantsServiceI)) {
+			tenantsServiceI = SpringContextUtil.getBean(TenantsServiceI.class);
+		}
 	}
 
 }
