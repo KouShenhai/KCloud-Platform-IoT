78a1cb6cc3bb1bd024bfb7ac0635b37394245b46
koushenhai <2413176044@qq.com>
2024-11-29 10:33:01 +0800

    feat: 引入gradle

---
+++ laokou-gradle/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "org.laokou"
+version = "3.4.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
