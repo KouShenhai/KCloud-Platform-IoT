afe7ce595cd40c7382243456695f4c05a26d71e4
koushenhai <2413176044@qq.com>
2024-12-15 20:45:06 +0800

    feat: 提交seata server 2.2.0【seata.properties配置在nacos default命名空间】

---
+++ laokou-sample/laokou-sample-seata/laokou-sample-seata-server/src/main/java/org/apache/seata/server/SeataApp.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seata.server;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.laokou.common.i18n.utils.SslUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author spilledyear@outlook.com
+ */
+@EnableEncryptableProperties
+@SpringBootApplication(scanBasePackages = {"org.apache.seata"})
+public class SeataApp {
+
+	/// ```properties
+	/// -Dnacos.remote.client.rpc.tls.enable=true
+	/// -Dnacos.remote.client.rpc.tls.mutualAuth=true
+	/// -Dnacos.remote.client.rpc.tls.certChainFile=nacos-client-cert.pem
+	/// -Dnacos.remote.client.rpc.tls.certPrivateKey=nacos-client-key.pem
+	/// -Dnacos.remote.client.rpc.tls.trustCollectionChainPath=nacos-ca-cert.pem
+	/// -Dnacos.remote.client.rpc.tls.certPrivateKeyPassword=laokou123
+	/// ```
+	public static void main(String[] args) {
+		// 忽略SSL认证
+		SslUtil.ignoreSSLTrust();
+		// 配置关闭nacos日志，因为nacos的log4j2导致本项目的日志不输出的问题
+		System.setProperty("nacos.logging.default.config.enabled", "false");
+		SpringApplication.run(SeataApp.class, args);
+	}
+
+}
