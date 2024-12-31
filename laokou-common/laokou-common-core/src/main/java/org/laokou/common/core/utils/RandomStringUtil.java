8d5c2cfda0db590decbab81a6a43ea371be94b48
koushenhai <2413176044@qq.com>
2024-12-31 13:48:26 +0800

    feat: 提交代码

--- laokou-common/laokou-common-core/src/main/java/org/laokou/common/core/utils/RandomStringUtil.java
+++ laokou-common/laokou-common-core/src/main/java/org/laokou/common/core/utils/RandomStringUtil.java
@@ -86,70 +86,6 @@ public final class RandomStringUtil {
 		super();
 	}
 
-	// Random
-	// -----------------------------------------------------------------------
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of all characters.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @return the random string
-	 */
-	public static String random(int count) {
-		return random(count, false, false);
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of characters whose ASCII value is between
-	 * <code>32</code> and <code>126</code> (inclusive).
-	 * </p>
-	 * @param count the length of random string to create
-	 * @return the random string
-	 */
-	public static String randomAscii(int count) {
-		return random(count, 32, 127, false, false);
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of alphabetic characters.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @return the random string
-	 */
-	public static String randomAlphabetic(int count) {
-		return random(count, true, false);
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of alpha-numeric characters.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @return the random string
-	 */
-	public static String randomAlphanumeric(int count) {
-		return random(count, true, true);
-	}
-
 	/**
 	 * <p>
 	 * Creates a random string whose length is the number of characters specified.
@@ -205,33 +141,6 @@ public final class RandomStringUtil {
 	 */
 	public static String random(int count, int start, int end, boolean letters, boolean numbers) {
 		return random(count, start, end, letters, numbers, null, RANDOM);
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string based on a variety of options, using default source of
-	 * randomness.
-	 * </p>
-	 *
-	 * <p>
-	 * This method has exactly the same semantics as
-	 * {@link #random(int, int, int, boolean, boolean, char[], Random)}, but instead of
-	 * using an externally supplied source of randomness, it uses the internal static
-	 * {@link Random} instance.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @param start the position in set of chars to start at
-	 * @param end the position in set of chars to end before
-	 * @param letters only allow letters?
-	 * @param numbers only allow numbers?
-	 * @param chars the set of chars to choose randoms from. If <code>null</code>, then it
-	 * will use the set of all chars.
-	 * @return the random string
-	 * @throws ArrayIndexOutOfBoundsException if there are not
-	 * <code>(end - start) + 1</code> characters in the set array.
-	 */
-	public static String random(int count, int start, int end, boolean letters, boolean numbers, char[] chars) {
-		return random(count, start, end, letters, numbers, chars, RANDOM);
 	}
 
 	/**
@@ -334,47 +243,6 @@ public final class RandomStringUtil {
 			}
 		}
 		return new String(buffer);
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of characters specified.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @param chars the String containing the set of characters to use, may be null
-	 * @return the random string
-	 * @throws IllegalArgumentException if <code>count</code> &lt; 0.
-	 */
-	public static String random(int count, String chars) {
-		if (chars == null) {
-			return random(count, 0, 0, false, false, null, RANDOM);
-		}
-		return random(count, chars.toCharArray());
-	}
-
-	/**
-	 * <p>
-	 * Creates a random string whose length is the number of characters specified.
-	 * </p>
-	 *
-	 * <p>
-	 * Characters will be chosen from the set of characters specified.
-	 * </p>
-	 * @param count the length of random string to create
-	 * @param chars the character array containing the set of characters to use, may be
-	 * null
-	 * @return the random string
-	 * @throws IllegalArgumentException if <code>count</code> &lt; 0.
-	 */
-	public static String random(int count, char[] chars) {
-		if (chars == null) {
-			return random(count, 0, 0, false, false, null, RANDOM);
-		}
-		return random(count, 0, chars.length, false, false, chars, RANDOM);
 	}
 
 }
