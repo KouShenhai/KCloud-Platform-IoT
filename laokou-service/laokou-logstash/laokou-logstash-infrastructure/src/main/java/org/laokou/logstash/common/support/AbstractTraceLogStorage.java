13b995cbf5b19620959cba16164f2ec79c872573
koushenhai <2413176044@qq.com>
2025-01-01 16:25:08 +0800

    feat: 优化代码[修改虚拟线程乱用或不合理的地方]

--- laokou-service/laokou-logstash/laokou-logstash-infrastructure/src/main/java/org/laokou/logstash/common/support/AbstractTraceLogStorage.java
+++ laokou-service/laokou-logstash/laokou-logstash-infrastructure/src/main/java/org/laokou/logstash/common/support/AbstractTraceLogStorage.java
@@ -17,17 +17,12 @@
 
 package org.laokou.logstash.common.support;
 
-import org.laokou.common.core.utils.ThreadUtil;
 import org.laokou.common.i18n.common.constant.StringConstant;
 import org.laokou.common.i18n.utils.DateUtil;
-
-import java.util.concurrent.ExecutorService;
 
 public abstract class AbstractTraceLogStorage implements TraceLogStorage {
 
 	protected static final String TRACE_INDEX = "laokou_trace";
-
-	protected static final ExecutorService EXECUTOR = ThreadUtil.newVirtualTaskExecutor();
 
 	protected String getIndexName() {
 		return TRACE_INDEX + StringConstant.UNDER + DateUtil.format(DateUtil.nowDate(), DateUtil.YYYYMMDD);
