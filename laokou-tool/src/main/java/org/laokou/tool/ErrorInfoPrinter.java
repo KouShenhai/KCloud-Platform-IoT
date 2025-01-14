30cdedde05014c927d8b8d4fbe85b309baf1ad3b
koushenhai <2413176044@qq.com>
2025-01-14 18:48:18 +0800

    feat: 提交错误打印

--- laokou-tool/src/main/java/org/laokou/tool/ErrorInfoPrinter.java
+++ laokou-tool/src/main/java/org/laokou/tool/ErrorInfoPrinter.java
@@ -17,19 +17,43 @@
 
 package org.laokou.tool;
 
-import lombok.extern.slf4j.Slf4j;
+import org.laokou.common.core.utils.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.laokou.common.i18n.common.constant.StringConstant.SLASH;
 
 /**
  * @author laokou
  */
-@Slf4j
 final class ErrorInfoPrinter {
 
 	private ErrorInfoPrinter() {
+
 	}
 
-	public static void main(String[] args) {
-		log.info("Hello World!");
+	public static void main(String[] args) throws IOException {
+		// 应用名称
+		String appName = "generator";
+		// 服务ID
+		String serviceId = "laokou-generator";
+		// JSON文件名称
+		String jsonName = String.format("%s.json", serviceId);
+		// 项目目录路径
+		String projectPath = System.getProperty("user.dir");
+		// 日志目录路径
+		String logPath = projectPath + SLASH + "logs";
+		// JSON文件路径
+		String jsonPath = logPath + SLASH + appName + SLASH + jsonName;
+		// 临时文件路径
+		String tempPath = projectPath + SLASH + "temp";
+		// 创建临时文件
+		FileUtil.create(tempPath, jsonName);
+		// 写入内容到临时文件
+		File file = new File(tempPath, jsonName);
+		FileUtil.write(file, FileUtil.getBytes(Path.of(jsonPath)));
 	}
 
 }
