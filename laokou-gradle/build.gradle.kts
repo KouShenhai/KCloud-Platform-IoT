61c9bedd01abce1ac4fd693c917d448ab5a03eb1
koushenhai <2413176044@qq.com>
2025-02-28 18:24:36 +0800

    feat: 增加dubbo/flink/spark/clickhose/starrocks/spring ai依赖

--- laokou-gradle/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {}
-
-allprojects {
-	repositories {
-		maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-		maven("https://repo.spring.io/release")
-		mavenCentral()
-	}
-}
