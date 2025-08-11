c8cfa1256859846345ca20f77ba342acae796543
koushenhai <2413176044@qq.com>
2025-08-11 18:02:20 +0800

    feat: 提交网关日志配置代码【go】

--- gateway/core/log.go
+++ gateway/core/log.go
@@ -18,24 +18,90 @@
 package core
 
 import (
+	"errors"
+	"github.com/DeRuina/timberjack"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
 	"time"
 )
 
-type LogConfig struct {
-	// 文件路径
-	FilePath string `yaml:"filePath"`
+const (
+	PROD = "prod"
+)
 
-	MaxSize          int       `yaml:"maxSize"`
-	MaxBackups       int       `yaml:"maxBackups"`
-	MaxAge           int       `yaml:"maxAge"`
-	Compress         bool      `yaml:"compress"`
-	LocalTime        bool      `yaml:"localTime"`
-	RotationInterval time.Time `yaml:"rotationInterval"`
-	RotateAtMinutes  []int     `yaml:"rotateAtMinutes"`
-	BackupTimeFormat string    `yaml:"backupTimeFormat"`
+type LogConfig struct {
+	// 日志级别
+	Level string `yaml:"level"`
+	// 环境
+	Profile string `yaml:"profile"`
+	// 日志格式
+	Pattern string `yaml:"pattern"`
+	// 日志文件路径
+	FilePath string `yaml:"file-path"`
+	// 日志最大容量【单位M】
+	MaxSize int `yaml:"max-size"`
+	// 日志最大数量
+	MaxBackups int `yaml:"max-backups"`
+	// 日志保留时间【单位天】
+	MaxAge int `yaml:"max-age"`
+	// 是否压缩
+	Compress bool `yaml:"compress"`
+	// 本地时间，默认值：false（使用UTC）
+	LocalTime bool `yaml:"local-time"`
+	// json格式
+	JsonFormat bool `yaml:"json-format"`
+	// 转换频率
+	RotationInterval time.Duration `yaml:"rotation-interval"`
+	// 转换时间【分钟】
+	RotateAtMinutes []int `yaml:"rotate-at-minutes"`
+	// 日志时间格式
+	BackupTimeFormat string `yaml:"backup-time-format"`
 }
 
-func (c *LogConfig) InitLogger() *zap.Logger {
-	return nil
+func (c *LogConfig) InitLogger() (*zap.Logger, error) {
+	timberjackLogger := &timberjack.Logger{
+		Filename:         c.FilePath,
+		MaxSize:          c.MaxSize,
+		MaxBackups:       c.MaxBackups,
+		MaxAge:           c.MaxAge,
+		Compress:         c.Compress,
+		LocalTime:        c.LocalTime,
+		RotationInterval: c.RotationInterval,
+		RotateAtMinutes:  c.RotateAtMinutes,
+		BackupTimeFormat: c.BackupTimeFormat,
+	}
+	log.SetOutput(timberjackLogger)
+	defer timberjackLogger.Close()
+	writeSyncer := zapcore.AddSync(timberjackLogger)
+	// 配置日志级别
+	levelConfig := zap.NewAtomicLevel()
+	level, err := zapcore.ParseLevel(c.Level)
+	if err != nil {
+		return nil, errors.New("日志级别不存在，请重新配置，错误信息：" + err.Error())
+	}
+	levelConfig.SetLevel(level)
+	// 配置环境
+	var encoderConfig zapcore.EncoderConfig
+	switch c.Profile {
+	case PROD:
+		encoderConfig = zap.NewProductionEncoderConfig()
+	default:
+		encoderConfig = zap.NewDevelopmentEncoderConfig()
+	}
+	// 设置时间格式
+	encoderConfig.EncodeTime = c.customTimeEncoder
+	var encoder zapcore.Encoder
+	// Json格式
+	if c.JsonFormat {
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	}
+	core := zapcore.NewCore(encoder, writeSyncer, levelConfig)
+	return zap.New(core, zap.AddCaller()), nil
+}
+
+func (c *LogConfig) customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format(c.Pattern))
 }
