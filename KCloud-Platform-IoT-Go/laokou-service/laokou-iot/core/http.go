1fd9b119250c9df2bff01255ddb9c7d7126ef1fd
koushenhai <2413176044@qq.com>
2025-08-12 12:29:23 +0800

    feat: 支持go客户端文件上传

--- KCloud-Platform-IoT-Go/laokou-service/laokou-iot/core/http.go
+++ KCloud-Platform-IoT-Go/laokou-service/laokou-iot/core/http.go
@@ -18,16 +18,18 @@
 package core
 
 import (
+	"bytes"
 	"crypto/tls"
 	"errors"
 	"io"
+	"mime/multipart"
 	"net/http"
 )
 
 func SendRequest(method, url string, param io.Reader, header map[string]string) (*http.Response, error) {
 	request, err := http.NewRequest(method, url, param)
 	if err != nil {
-		return nil, errors.New("创建Request失败，错误信息：" + err.Error())
+		return nil, errors.New("创建 Request 失败，错误信息：" + err.Error())
 	}
 	if header != nil {
 		for k, v := range header {
@@ -37,14 +39,35 @@ func SendRequest(method, url string, param io.Reader, header map[string]string) 
 	return sendHttpRequest(request)
 }
 
+func GetFormFile(fileName string, buf []byte) (io.Reader, string, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", fileName)
+	if err != nil {
+		return nil, "", errors.New("创建 FormFile 失败，错误信息：" + err.Error())
+	}
+	_, err = io.Copy(part, bytes.NewReader(buf))
+	if err != nil {
+		return nil, "", errors.New("复制字节数组失败，错误信息：" + err.Error())
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, "", errors.New("关闭 Writer 失败，错误信息：" + err.Error())
+	}
+	return body, writer.FormDataContentType(), nil
+}
+
 func SendRequestAndGetBody(method, url string, param io.Reader, header map[string]string) ([]byte, error) {
 	response, err := SendRequest(method, url, param, header)
 	if err != nil {
 		return nil, err
 	}
 	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.New("读取响应失败，错误信息：" + err.Error())
+	}
 	defer response.Body.Close()
-	return body, err
+	return body, nil
 }
 
 func sendHttpRequest(request *http.Request) (*http.Response, error) {
@@ -55,7 +78,7 @@ func sendHttpRequest(request *http.Request) (*http.Response, error) {
 	}
 	response, err := client.Do(request)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("发送 HTTP 请求失败，错误信息：" + err.Error())
 	}
 	return response, nil
 }
