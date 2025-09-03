1f69143e3ed4b3add69ed78e7f2f63399c4874f9
koushenhai <2413176044@qq.com>
2025-09-03 19:07:09 +0800

    fix: 提交modbus4j

---
+++ laokou-common/laokou-common-modbus4j/src/main/java/org/laokou/common/modbus4j/config/AbstractModbus.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022-2025 KCloud-Platform-IoT Author or Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.laokou.common.modbus4j.config;
+
+import com.serotonin.modbus4j.ModbusMaster;
+import com.serotonin.modbus4j.exception.ModbusInitException;
+import com.serotonin.modbus4j.exception.ModbusTransportException;
+import com.serotonin.modbus4j.msg.*;
+
+public abstract class AbstractModbus implements Modbus {
+
+	protected final SpringModbusProperties properties;
+
+	protected volatile ModbusMaster modbusMaster;
+
+	protected AbstractModbus(SpringModbusProperties properties) {
+		this.properties = properties;
+	}
+
+	@Override
+	public synchronized void open() throws ModbusInitException {
+		modbusMaster.setTimeout(properties.getTimeout());
+		modbusMaster.setRetries(properties.getRetries());
+		modbusMaster.init();
+	}
+
+	@Override
+	public synchronized void close() {
+		modbusMaster.destroy();
+	}
+
+	@Override
+	public ModbusResponse sendReadHoldingRegistersRequest(int slaveId, int startOffset, int numberOfRegisters)
+			throws ModbusTransportException {
+		return modbusMaster.send(new ReadHoldingRegistersRequest(slaveId, startOffset, numberOfRegisters));
+	}
+
+	@Override
+	public ModbusResponse sendReadInputRegistersRequest(int slaveId, int startOffset, int numberOfRegisters)
+			throws ModbusTransportException {
+		return modbusMaster.send(new ReadInputRegistersRequest(slaveId, startOffset, numberOfRegisters));
+	}
+
+	@Override
+	public ModbusResponse sendReadCoilsRequest(int slaveId, int startOffset, int numberOfRegisters)
+			throws ModbusTransportException {
+		return modbusMaster.send(new ReadCoilsRequest(slaveId, startOffset, numberOfRegisters));
+	}
+
+	@Override
+	public ModbusResponse sendReadDiscreteInputsRequest(int slaveId, int startOffset, int numberOfRegisters)
+			throws ModbusTransportException {
+		return modbusMaster.send(new ReadDiscreteInputsRequest(slaveId, startOffset, numberOfRegisters));
+	}
+
+}
---
+++ laokou-common/laokou-common-modbus4j/src/main/java/org/laokou/common/modbus4j/config/ModbusTypeEnum.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022-2025 KCloud-Platform-IoT Author or Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.laokou.common.modbus4j.config;
+
+import com.serotonin.modbus4j.ModbusFactory;
+import lombok.Getter;
+import org.laokou.common.i18n.util.EnumParser;
+
+/**
+ * @author laokou
+ */
+@Getter
+public enum ModbusTypeEnum {
+
+	RTU_MASTER("rtu_master", "RTU MASTER") {
+		@Override
+		public Modbus getModbus(ModbusFactory modbusFactory, SpringModbusProperties properties) {
+			return new ModbusRtuMaster(modbusFactory, properties);
+		}
+	},
+
+	TCP_MASTER("tcp_master", "TCP MASTER") {
+		@Override
+		public Modbus getModbus(ModbusFactory modbusFactory, SpringModbusProperties properties) {
+			return new ModbusTcpMaster(modbusFactory, properties);
+		}
+	},
+
+	ASCII_MASTER("ascii_master", "ASCII MASTER") {
+		@Override
+		public Modbus getModbus(ModbusFactory modbusFactory, SpringModbusProperties properties) {
+			return new ModbusAsciiMaster(modbusFactory, properties);
+		}
+	},
+
+	UDP_MASTER("udp_master", "UDP MASTER") {
+		@Override
+		public Modbus getModbus(ModbusFactory modbusFactory, SpringModbusProperties properties) {
+			return new ModbusUdpMaster(modbusFactory, properties);
+		}
+	};
+
+	private final String code;
+
+	private final String desc;
+
+	ModbusTypeEnum(String code, String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
+
+	public static ModbusTypeEnum getByCode(String code) {
+		return EnumParser.parse(ModbusTypeEnum.class, ModbusTypeEnum::getCode, code);
+	}
+
+	public abstract Modbus getModbus(ModbusFactory modbusFactory, SpringModbusProperties properties);
+
+}
