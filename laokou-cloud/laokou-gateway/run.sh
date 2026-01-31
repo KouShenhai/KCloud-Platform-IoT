02876a9aff5ff5e802e1923d75134c0db0f89540
laokou【k↑】 <2413176044@qq.com>
2026-01-31 16:57:17 +0800

    feat: 构建laokou-gateway native image

--- laokou-cloud/laokou-gateway/run.sh
+++
@@ -1,2 +0,0 @@
-docker pull registry.cn-shenzhen.aliyuncs.com/koushenhai/laokou-gateway-native:4.0.1-SNAPSHOT
-docker run -d -p 5555:5555 --net=host registry.cn-shenzhen.aliyuncs.com/koushenhai/laokou-gateway-native:4.0.1-SNAPSHOT
