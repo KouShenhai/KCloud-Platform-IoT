bbfeedcab5aa1f1fb6bd2598c28c8591276873a7
koushenhai <2413176044@qq.com>
2026-02-27 11:20:51 +0800

    feat: 升级es版本

--- laokou-common/laokou-common-elasticsearch/src/main/java/org/laokou/common/elasticsearch/config/SpringElasticsearchProperties.java
+++ laokou-common/laokou-common-elasticsearch/src/main/java/org/laokou/common/elasticsearch/config/SpringElasticsearchProperties.java
@@ -53,9 +53,9 @@ public class SpringElasticsearchProperties {
 
 	private String proxy;
 
-	private String version = "9.3.0";
+	private String version = "9.3.1";
 
-	private String clientVersion = "9.3.0";
+	private String clientVersion = "9.3.1";
 
 	private final RestClient restClient = new RestClient();
 
