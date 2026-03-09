cb4992fb4dc1f47242b0e8a5e3c1a5ca13a85ede
laokou【k↑】 <2413176044@qq.com>
2026-03-09 23:40:39 +0800

    feat: 使用liquibase管理数据库版本

---
+++ laokou-service/laokou-admin/laokou-admin-start/src/main/resources/db/changelog/sql/000002_insert_i18n_menu.sql
@@ -0,0 +1,32 @@
+-- liquibase formatted sql
+
+-- changeset dev:000002
+INSERT INTO "public"."sys_i18n_menu" VALUES (8, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.iot.device.thingModel', '物模型');
+INSERT INTO "public"."sys_i18n_menu" VALUES (10, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.iot.device.productCategory', '产品类别');
+INSERT INTO "public"."sys_i18n_menu" VALUES (1, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.sys', '系统管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (4, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.sys.log.notice', '通知日志');
+INSERT INTO "public"."sys_i18n_menu" VALUES (3, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.sys.log.login', '登录日志');
+INSERT INTO "public"."sys_i18n_menu" VALUES (2, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.sys.log', '日志管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (5, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.iot', '物联管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (6, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.iot.device', '设备管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (11, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.permission', '权限管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (12, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.permission.menu', '菜单');
+INSERT INTO "public"."sys_i18n_menu" VALUES (13, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.permission.dept', '部门');
+INSERT INTO "public"."sys_i18n_menu" VALUES (14, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.permission.role', '角色');
+INSERT INTO "public"."sys_i18n_menu" VALUES (15, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.permission.user', '用户');
+INSERT INTO "public"."sys_i18n_menu" VALUES (9, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.iot.device.product', '产品');
+INSERT INTO "public"."sys_i18n_menu" VALUES (7, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505,  'menu.iot.device.device', '设备');
+INSERT INTO "public"."sys_i18n_menu" VALUES (16, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.tenant', '租户管理');
+INSERT INTO "public"."sys_i18n_menu" VALUES (17, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.tenant.source', '数据源');
+INSERT INTO "public"."sys_i18n_menu" VALUES (18, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.tenant.package', '套餐');
+INSERT INTO "public"."sys_i18n_menu" VALUES (19, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.tenant.tenant', '租户');
+INSERT INTO "public"."sys_i18n_menu" VALUES (20, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.base', '基础数据');
+INSERT INTO "public"."sys_i18n_menu" VALUES (21, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.base.dict', '数据字典');
+INSERT INTO "public"."sys_i18n_menu" VALUES (22, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.log.operate', '操作日志');
+INSERT INTO "public"."sys_i18n_menu" VALUES (23, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.base.i18n-menu', '国际化菜单');
+INSERT INTO "public"."sys_i18n_menu" VALUES (24, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.oss', '对象存储');
+INSERT INTO "public"."sys_i18n_menu" VALUES (25, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.oss.config', '对象存储配置');
+INSERT INTO "public"."sys_i18n_menu" VALUES (26, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.oss.log', '对象存储日志');
+INSERT INTO "public"."sys_i18n_menu" VALUES (28, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.config', '系统配置');
+INSERT INTO "public"."sys_i18n_menu" VALUES (29, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.config.generator', '代码生成器');
+INSERT INTO "public"."sys_i18n_menu" VALUES (27, 1, 1, '2026-03-07 12:06:37', '2026-03-07 12:06:37', 0, 0, 0, 1535887940687765505, 'menu.sys.cluster', '集群管理');
