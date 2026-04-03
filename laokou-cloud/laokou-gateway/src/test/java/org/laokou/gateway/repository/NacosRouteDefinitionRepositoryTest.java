26a0681fbcee77e66f5bbe171dc8ca2a24a81ce5
KouShenhai <2413176044@qq.com>
2026-04-03 18:52:25 +0800

    feat: 提交代码

--- laokou-cloud/laokou-gateway/src/test/java/org/laokou/gateway/repository/NacosRouteDefinitionRepositoryTest.java
+++ laokou-cloud/laokou-gateway/src/test/java/org/laokou/gateway/repository/NacosRouteDefinitionRepositoryTest.java
@@ -17,12 +17,19 @@
 
 package org.laokou.gateway.repository;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
 import com.redis.testcontainers.RedisStackContainer;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.laokou.common.testcontainers.container.NacosContainer;
 import org.laokou.common.testcontainers.util.DockerImageNames;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -30,6 +37,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
  * @author laokou
  */
 @Testcontainers
+@EnableDiscoveryClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+		classes = { com.alibaba.cloud.nacos.NacosConfigAutoConfiguration.class })
 @DisplayName("Nacos Route Definition Repository Tests")
 public class NacosRouteDefinitionRepositoryTest {
 
@@ -39,9 +49,28 @@ public class NacosRouteDefinitionRepositoryTest {
 	@Container
 	static RedisStackContainer redisContainer = new RedisStackContainer(DockerImageNames.redis());
 
+	private NacosConfigManager nacosConfigManager;
+
+	private ReactiveRedisTemplate<@NonNull String, @NonNull Object> reactiveRedisTemplate;
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redisContainer::getHost);
+		registry.add("spring.data.redis.port", redisContainer::getFirstMappedPort);
+		registry.add("spring.cloud.nacos.discovery.server-addr", nacosContainer::getServerAddr);
+		registry.add("spring.cloud.nacos.config.server-addr", nacosContainer::getServerAddr);
+		registry.add("spring.cloud.nacos.discovery.namespace", () -> "public");
+		registry.add("spring.cloud.nacos.config.namespace", () -> "public");
+		registry.add("spring.cloud.nacos.discovery.group", () -> "DEFAULT_GROUP");
+		registry.add("spring.cloud.nacos.config.group", () -> "DEFAULT_GROUP");
+		registry.add("spring.cloud.nacos.discovery.username", () -> "nacos");
+		registry.add("spring.cloud.nacos.config.username", () -> "nacos");
+		registry.add("spring.cloud.nacos.discovery.password", () -> "nacos");
+		registry.add("spring.cloud.nacos.config.password", () -> "nacos");
+	}
+
 	@BeforeAll
 	static void setUp() {
-
 	}
 
 	@Test
