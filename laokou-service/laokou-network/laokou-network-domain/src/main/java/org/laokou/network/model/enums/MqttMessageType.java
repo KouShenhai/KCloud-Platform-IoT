8198346479445825a00ff3692c3f5bb95f1e22df
laokou【k↑】 <2413176044@qq.com>
2026-05-18 07:55:00 +0800

    style: 优化代码风格

--- laokou-service/laokou-network/laokou-network-domain/src/main/java/org/laokou/network/model/enums/MqttMessageType.java
+++ laokou-service/laokou-network/laokou-network-domain/src/main/java/org/laokou/network/model/enums/MqttMessageType.java
@@ -246,11 +246,16 @@ public enum MqttMessageType {
 
 	public abstract String getMqTopic();
 
+	private static final MqttMessageType[] VALUES = values();
+
 	public static Map<String, Integer> getTopics(Long tenantId, Integer qos) {
-		MqttMessageType[] values = values();
-		Map<String, Integer> topics = Maps.newHashMapWithExpectedSize(values.length);
-		for (MqttMessageType messageType : values) {
-			topics.put(messageType.getTopic().replaceFirst("\\+", tenantId.toString()), qos);
+		Map<String, Integer> topics = Maps.newHashMapWithExpectedSize(VALUES.length);
+		for (MqttMessageType messageType : VALUES) {
+			String topic = messageType.getTopic();
+			int plusIndex = topic.indexOf('+');
+			String replacedTopic = plusIndex >= 0
+					? topic.substring(0, plusIndex) + tenantId.toString() + topic.substring(plusIndex + 1) : topic;
+			topics.put(replacedTopic, qos);
 		}
 		return topics;
 	}
