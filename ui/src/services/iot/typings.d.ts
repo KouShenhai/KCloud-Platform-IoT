cc3b2354e011cbe0829fc9d6d72048e2eb8f70c3
laokou <2413176044@qq.com>
2026-07-13 15:26:12 +0800

    feat: 新增数据源

--- ui/src/services/iot/typings.d.ts
+++ ui/src/services/iot/typings.d.ts
@@ -303,8 +303,8 @@ declare namespace API {
 		extValues?: Record<string, any>;
 		id?: number;
 		name?: string;
-		driverClassName?: string;
-		url?: string;
+		endpoint?: string;
+		type?: string;
 		username?: string;
 		password?: string;
 	};
