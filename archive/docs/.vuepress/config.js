fa5d4375af71264eff90e7884a1190794d94718a
laokou <2413176044@qq.com>
2026-09-07 12:54:04 +0800

    docs: 提交rabbitmq文档

--- archive/docs/.vuepress/config.js
+++ archive/docs/.vuepress/config.js
@@ -65,6 +65,7 @@ module.exports = {
 							{text: 'Ubuntu22.04安装Mysql9.6.0', link: '/pages/a0baf10/'},
 							{text: 'ARM64疑难杂症', link: '/pages/d9a8f5/'},
 							{text: 'FRP内网穿透', link: '/pages/01381a/'},
+							{text: 'Docker安装RabbitMQ', link: '/pages/4ae44e/'},
 						]
 					},
 					{
