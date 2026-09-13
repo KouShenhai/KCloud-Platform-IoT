1e71b46229743735b1cd0c954428e392bc6bb481
laokou <2413176044@qq.com>
2026-09-13 14:27:52 +0800

    style: 优化代码风格

---
+++ laokou-service/laokou-ai/laokou-ai-mcp-gateway/src/main/java/org/laokou/mcp/gateway/McpGatewayApp.java
@@ -0,0 +1,13 @@
+package org.laokou.mcp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaokouAiMcpGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaokouAiMcpGatewayApplication.class, args);
+	}
+
+}
